b566b52bdaf3e4c75b92b78b69568dad4f47d382
Shawn O. Pearce <sop@google.com>
2012-05-10 19:04:38 -0700

    Initial replication plugin, split from Gerrit Code Review
    This plugin provides server driven replication within the Gerrit
    process using the JGit and JSch client libraries to push reference
    updates to remote destinations using the Git protocol.
    
    Taken from gerrit 5de34d9713108fb46e74c12b96db2e3e6be3c672.
    
    Change-Id: Id09595d4371e999d175268314d2e320f89839ef6

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/OnStartStop.java
@@ -0,0 +1,61 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import com.google.common.util.concurrent.Atomics;
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.extensions.systemstatus.ServerInformation;
+import com.google.inject.Inject;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+class OnStartStop implements LifecycleListener {
+  private final AtomicReference<Future<?>> pushAllFuture;
+  private final ServerInformation srvInfo;
+  private final PushAll.Factory pushAll;
+  private final ReplicationQueue queue;
+
+  @Inject
+  OnStartStop(
+      ServerInformation srvInfo,
+      PushAll.Factory pushAll,
+      ReplicationQueue queue) {
+    this.srvInfo = srvInfo;
+    this.pushAll = pushAll;
+    this.queue = queue;
+    this.pushAllFuture = Atomics.newReference();
+  }
+
+  @Override
+  public void start() {
+    queue.start();
+
+    if (srvInfo.getState() == ServerInformation.State.STARTUP
+        && queue.replicateAllOnPluginStart) {
+      pushAllFuture.set(pushAll.create(null).schedule(30, TimeUnit.SECONDS));
+    }
+  }
+
+  @Override
+  public void stop() {
+    Future<?> f = pushAllFuture.getAndSet(null);
+    if (f != null) {
+      f.cancel(true);
+    }
+    queue.stop();
+  }
+}
---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/SecureCredentialsFactory.java
@@ -0,0 +1,59 @@
+// Copyright (C) 2011 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.util.FS;
+
+import java.io.IOException;
+
+/** Looks up a remote's password in secure.config. */
+class SecureCredentialsFactory {
+  private final Config config;
+
+  @Inject
+  SecureCredentialsFactory(SitePaths site)
+      throws ConfigInvalidException, IOException {
+    config = load(site);
+  }
+
+  private static Config load(SitePaths site)
+      throws ConfigInvalidException, IOException {
+    FileBasedConfig cfg = new FileBasedConfig(site.secure_config, FS.DETECTED);
+    if (cfg.getFile().exists() && cfg.getFile().length() > 0) {
+      try {
+        cfg.load();
+      } catch (ConfigInvalidException e) {
+        throw new ConfigInvalidException(String.format(
+            "Config file %s is invalid: %s", cfg.getFile(), e.getMessage()), e);
+      } catch (IOException e) {
+        throw new IOException(String.format(
+            "Cannot read %s: %s", cfg.getFile(), e.getMessage()), e);
+      }
+    }
+    return cfg;
+  }
+
+  SecureCredentialsProvider create(String remoteName) {
+    String user = config.getString("remote", remoteName, "username");
+    String pass = config.getString("remote", remoteName, "password");
+    return new SecureCredentialsProvider(user, pass);
+  }
+}
