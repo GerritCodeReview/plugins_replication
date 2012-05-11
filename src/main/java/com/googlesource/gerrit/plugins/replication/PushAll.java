050de4bf79d2d02e0e483a2eb14c6403d5a5f149
Shawn O. Pearce <sop@google.com>
2012-05-10 19:04:38 -0700

    Initial replication plugin, split from Gerrit Code Review
    This plugin provides server driven replication within the Gerrit
    process using the JGit and JSch client libraries to push reference
    updates to remote destinations using the Git protocol.
    
    Taken from gerrit 5de34d9713108fb46e74c12b96db2e3e6be3c672.
    
    Change-Id: Id09595d4371e999d175268314d2e320f89839ef6

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/PushAll.java
@@ -0,0 +1,68 @@
+// Copyright (C) 2009 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.git.DefaultQueueOp;
+import com.google.gerrit.server.git.WorkQueue;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+
+class PushAll extends DefaultQueueOp {
+  interface Factory {
+    PushAll create(String urlMatch);
+  }
+
+  private static final Logger log =
+      LoggerFactory.getLogger(PushAll.class);
+
+  private final ProjectCache projectCache;
+  private final RunningReplicationQueue replication;
+  private final String urlMatch;
+
+  @Inject
+  PushAll(final WorkQueue wq, final ProjectCache projectCache,
+      final RunningReplicationQueue rq, @Assisted @Nullable final String urlMatch) {
+    super(wq);
+    this.projectCache = projectCache;
+    this.replication = rq;
+    this.urlMatch = urlMatch;
+  }
+
+  public void run() {
+    try {
+      for (final Project.NameKey nameKey : projectCache.all()) {
+        replication.scheduleFullSync(nameKey, urlMatch);
+      }
+    } catch (RuntimeException e) {
+      log.error("Cannot enumerate known projects", e);
+    }
+  }
+
+  @Override
+  public String toString() {
+    String s = "Replicate All Projects";
+    if (urlMatch != null) {
+      s = s + " to " + urlMatch;
+    }
+    return s;
+  }
+}
