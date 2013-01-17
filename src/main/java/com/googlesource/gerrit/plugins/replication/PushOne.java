a18c721858b0feb13f1ee05c191915bcd461a588
Gustaf Lundh <gustaf.lundh@sonymobile.com>
2013-01-17 14:07:14 +0100

    Retry replications failed due to "failed to lock" errors
    If two or more replication PushOps (to the same GIT and Ref) are
    scheduled at approximately same time (and end up on different
    replication threads), there is a large probability that the last
    push to complete will fail with a remote "failure to lock" error.
    
    This is due to the first replication operation updating the very
    same remote ref during the execution of the second replication push
    operation.
    
    Since this is not recognized as a transient issue, the last ref
    update will never happen and the commit will not be reachable from
    the Gerrit slave.
    
    In a very active system as ours, this happens often enough to be a
    huge issue for both users and CI-systems.
    
    This fix will acknowledge these "failed to lock" issues as a
    transient error and schedule the failed replication operation
    for a retry (up to a total 3 retries).
    
    Change-Id: Ic51d8407e38eea257b0a2e5fb529cf23b5875ca8

--- src/main/java/com/googlesource/gerrit/plugins/replication/PushOne.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/PushOne.java
@@ -104,6 +104,7 @@ class PushOne implements ProjectRunnable {
   private boolean canceled;
   private final Multimap<String,ReplicationState> stateMap =
       LinkedListMultimap.create();
+  private int lockRetry;
 
   @Inject
   PushOne(final GitRepositoryManager grm,
@@ -128,6 +129,7 @@ class PushOne implements ProjectRunnable {
     replicationQueue = rq;
     projectName = d;
     uri = u;
+    lockRetry = 0;
   }
 
   @Override
@@ -293,15 +295,24 @@ class PushOne implements ProjectRunnable {
       if (cause instanceof JSchException
           && cause.getMessage().startsWith("UnknownHostKey:")) {
         log.error("Cannot replicate to " + uri + ": " + cause.getMessage());
+      } else if (e instanceof LockFailureException) {
+        lockRetry++;
+        // The LockFailureException message contains both URI and reason
+        // for this failure.
+        log.error("Cannot replicate to " + e.getMessage());
       } else {
         log.error("Cannot replicate to " + uri, e);
       }
 
       // The remote push operation should be retried.
-      pool.reschedule(this, Destination.RetryReason.TRANSPORT_ERROR);
+      if (lockRetry <= pool.getLockErrorMaxRetries()) {
+        pool.reschedule(this, Destination.RetryReason.TRANSPORT_ERROR);
+      } else {
+        log.error("Giving up after " + pool.getLockErrorMaxRetries() +
+            " attempts to replicate, but still failing due to: " + e.getMessage());
+      }
     } catch (IOException e) {
       wrappedLog.error("Cannot replicate to " + uri, e, getStatesAsArray());
-
     } catch (RuntimeException e) {
       wrappedLog.error("Unexpected error during replication to " + uri, e, getStatesAsArray());
 
@@ -507,7 +518,8 @@ class PushOne implements ProjectRunnable {
     cmds.add(new RemoteRefUpdate(git, (Ref) null, dst, force, null, null));
   }
 
-  private void updateStates(Collection<RemoteRefUpdate> refUpdates) {
+  private void updateStates(Collection<RemoteRefUpdate> refUpdates)
+      throws LockFailureException {
     Set<String> doneRefs = new HashSet<String>();
     boolean anyRefFailed = false;
 
@@ -543,6 +555,8 @@ class PushOne implements ProjectRunnable {
                 + ", remote rejected non-fast-forward push."
                 + "  Check receive.denyNonFastForwards variable in config file"
                 + " of destination repository.", u.getRemoteName(), uri), logStatesArray);
+          } else if ("failed to lock".equals(u.getMessage())) {
+            throw new LockFailureException(uri, u.getMessage());
           } else {
             wrappedLog.error(String.format(
                 "Failed replicate of %s to %s, reason: %s",
@@ -572,4 +586,12 @@ class PushOne implements ProjectRunnable {
     }
     stateMap.clear();
   }
+
+  public class LockFailureException extends TransportException {
+    private static final long serialVersionUID = 1L;
+
+    public LockFailureException(URIish uri, String message) {
+      super(uri, message);
+    }
+  };
 }
