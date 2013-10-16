f747d1ca463deb5b5684f71e331b2cf22989ab66
Hugo Ares <hugo.ares@ericsson.com>
2013-10-16 13:03:28 -0400

    Send stream events for ref replication
    Send replication events when a git reference is updated, currently not sending
    event for replication at start up replication command. Send ref-replicated event
    for each node the ref is done replicating to and send ref-replication-done event
    once replication of the ref is complete to all the nodes.
    
    The main goal of this change is to allow interested tools (e.g.Jenkins Gerrit
    Trigger) to kown when replication of a ref to a node is complete.
    
    Here is an example of an event when a ref is replicated to a specific node:
    
      {"type":"ref-replicated",
       "project":"testReplication",
       "ref":"refs/heads/master",
       "targetNode":"someHost",
       "status":"succeeded"}
    
    Here is an example of an event when a ref completed to all the nodes:
    
      {"type":"ref-replication-done",
       "project":"testReplication",
       "ref":"refs/heads/master",
       "nodesCount":5}
    
    Change-Id: Ic2163e0f28678cb484e4dba2989d08b93e3729b6

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/RefReplicationDoneEvent.java
@@ -0,0 +1,30 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import com.google.gerrit.server.events.ChangeEvent;
+
+public class RefReplicationDoneEvent extends ChangeEvent {
+  public final String type = "ref-replication-done";
+  public String project;
+  public String ref;
+  public int nodesCount;
+
+  public RefReplicationDoneEvent(String project, String ref, int nodesCount) {
+    this.project = project;
+    this.ref = ref;
+    this.nodesCount = nodesCount;
+  }
+}
