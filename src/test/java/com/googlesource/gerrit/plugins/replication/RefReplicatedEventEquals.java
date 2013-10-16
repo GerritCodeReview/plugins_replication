87553d712d56f48a90bb2b1f65c2ece2b8279f71
Hugo Ares <hugo.ares@ericsson.com>
2013-10-16 13:03:28 -0400

    Send stream events for ref replication
    Send replication events when a git reference is updated, currently not sending
    event for replication at plugin start up or when replication command is
    executed. Send ref-replicated event for each node the ref is done replicating
    to and send ref-replication-done event once replication of the ref is complete
    to all the nodes.
    
    The main goal of this change is to allow interested tools (e.g.Jenkins Gerrit
    Trigger) to know when replication of a ref to a node is complete.
    
    Here is an example of an event when a ref is replicated to a specific node:
    
      {"type":"ref-replicated",
       "project":"testReplication",
       "ref":"refs/heads/master",
       "targetNode":"someHost",
       "status":"succeeded"}
    
    Here is an example of an event when a ref is replicated to all the nodes:
    
      {"type":"ref-replication-done",
       "project":"testReplication",
       "ref":"refs/heads/master",
       "nodesCount":5}
    
    Change-Id: Ic2163e0f28678cb484e4dba2989d08b93e3729b6

---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/RefReplicatedEventEquals.java
@@ -0,0 +1,79 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import org.easymock.EasyMock;
+import org.easymock.IArgumentMatcher;
+
+public class RefReplicatedEventEquals implements IArgumentMatcher {
+
+  private RefReplicatedEvent expected;
+
+  public RefReplicatedEventEquals(RefReplicatedEvent expected) {
+    this.expected = expected;
+  }
+
+  public static final RefReplicatedEvent eqEvent(RefReplicatedEvent refReplicatedEvent) {
+    EasyMock.reportMatcher(new RefReplicatedEventEquals(refReplicatedEvent));
+    return null;
+  }
+
+  @Override
+  public boolean matches(Object actual) {
+    if (!(actual instanceof RefReplicatedEvent)) {
+      return false;
+    }
+    RefReplicatedEvent actualRefReplicatedEvent = (RefReplicatedEvent)actual;
+    if (!checkObjectsEquals(expected.project, actualRefReplicatedEvent.project)) {
+      return false;
+    }
+    if (!checkObjectsEquals(expected.ref, actualRefReplicatedEvent.ref)) {
+      return false;
+    }
+    if (!checkObjectsEquals(expected.targetNode,
+        actualRefReplicatedEvent.targetNode)) {
+      return false;
+    }
+    if (!checkObjectsEquals(expected.status, actualRefReplicatedEvent.status)) {
+      return false;
+    }
+    return true;
+  }
+
+  private boolean checkObjectsEquals(Object object1, Object object2) {
+    if (object1 == object2) {
+      return true;
+    }
+    if (object1 != null && !object1.equals(object2)) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public void appendTo(StringBuffer buffer) {
+    buffer.append("eqEvent(");
+    buffer.append(expected.getClass().getName());
+    buffer.append(" with project \"");
+    buffer.append(expected.project);
+    buffer.append(" and ref \"");
+    buffer.append(expected.ref);
+    buffer.append(" and targetNode \"");
+    buffer.append(expected.targetNode);
+    buffer.append(" and status \"");
+    buffer.append(expected.status);
+    buffer.append("\")");
+  }
+}
