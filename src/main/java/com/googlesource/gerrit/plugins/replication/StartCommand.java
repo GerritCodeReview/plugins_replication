a82cf9871cd7083ce9f62d07500d93194dc38132
Felix Hall <felix.hall@sonymobile.com>
2014-10-22 15:07:46 +0200

    Added regex and wildcard matching to ssh command
    Patterns can now be used in the list of projects to replicate.
    Actually queued projects are the intersection of patterns in
    replication.config and the project list argument
    
    This can be used e.g. when having large amounts of projects
    where only those in a certain sub directory are to be replicated,
    to avoid filling up the replication queue unnecessarily.
    
    Change-Id: I44fa3fbfca5ee9067dd288e3ed45a256e1955d50

--- src/main/java/com/googlesource/gerrit/plugins/replication/StartCommand.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/StartCommand.java
@@ -15,8 +15,6 @@
 package com.googlesource.gerrit.plugins.replication;
 
 import com.google.gerrit.extensions.annotations.RequiresCapability;
-import com.google.gerrit.reviewdb.client.Project;
-import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.sshd.CommandMetaData;
 import com.google.gerrit.sshd.SshCommand;
 import com.google.inject.Inject;
@@ -49,39 +47,30 @@ final class StartCommand extends SshCommand {
       usage = "wait for replication to finish before exiting")
   private boolean wait;
 
-  @Argument(index = 0, multiValued = true, metaVar = "PROJECT", usage = "project name")
-  private List<String> projectNames = new ArrayList<String>(2);
+  @Argument(index = 0, multiValued = true, metaVar = "PATTERN", usage = "project name pattern")
+  private List<String> projectPatterns = new ArrayList<String>(2);
 
   @Inject
-  private PushAll.Factory pushAllFactory;
-
-  @Inject
-  private ReplicationQueue replication;
-
-  @Inject
-  private ProjectCache projectCache;
+  private PushAll.Factory pushFactory;
 
   @Override
   protected void run() throws Failure {
-    if (all && projectNames.size() > 0) {
+    if (all && projectPatterns.size() > 0) {
       throw new UnloggedFailure(1, "error: cannot combine --all and PROJECT");
     }
 
     ReplicationState state = new ReplicationState(new CommandProcessing(this));
     Future<?> future = null;
+
+    ReplicationFilter projectFilter;
+
     if (all) {
-      future = pushAllFactory.create(urlMatch, state).schedule(0, TimeUnit.SECONDS);
+      projectFilter = ReplicationFilter.all();
     } else {
-      for (String name : projectNames) {
-        Project.NameKey key = new Project.NameKey(name);
-        if (projectCache.get(key) != null) {
-          replication.scheduleFullSync(key, urlMatch, state);
-        } else {
-          writeStdErrSync("error: '" + name + "': not a Gerrit project");
-        }
-      }
-      state.markAllPushTasksScheduled();
+      projectFilter = new ReplicationFilter(projectPatterns);
     }
+
+    future = pushFactory.create(urlMatch, projectFilter, state).schedule(0, TimeUnit.SECONDS);
 
     if (wait) {
       if (future != null) {
