e014409754d01d0a65a088e62e3a04a201b522e2
Dariusz Luksza <dariusz@luksza.org>
2015-03-04 10:44:31 +0100

    Expose more API to enable plugin consumption as lib
    In order to use replication plugin as a base for other replication
    plugins more API needs to be exposed.
    
    All classes bound in ReplicationModue are made public with package
    protected constructors to enable extendability.
    
    Also part of configuration API is exposed for supporting different
    configuration sources then $site_path/replication.config eg. dynamically
    load from 'master' server.
    
    Last thing is to enable consumers to modify SSH session eg. modify
    credentials factory and/or set know host repository. For this purpose
    ReplicationSshSessionFactory interface was added.
    
    Change-Id: I2e26fc976b04bd1ace5ddd695ef558c6d66a4716
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- src/main/java/com/googlesource/gerrit/plugins/replication/DefaultReplicationSshSessionFactory.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/DefaultReplicationSshSessionFactory.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2013 The Android Open Source Project
+// Copyright (C) 2015 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,13 +14,13 @@
 
 package com.googlesource.gerrit.plugins.replication;
 
-import com.google.gerrit.extensions.config.CapabilityDefinition;
+import org.eclipse.jgit.transport.SshSessionFactory;
 
-public class StartReplicationCapability extends CapabilityDefinition {
-  public static final String START_REPLICATION = "startReplication";
+class DefaultReplicationSshSessionFactory implements
+    ReplicationSshSessionFactory {
 
   @Override
-  public String getDescription() {
-    return "Start Replication";
+  public SshSessionFactory create() {
+    return SshSessionFactory.getInstance();
   }
 }
