af37afa9bf9c131a83bcbf675071dfd162dc7350
Dariusz Luksza <dariusz@luksza.org>
2015-03-04 10:44:31 +0100

    Expose more API to enable plugin consumption as lib
    In order to use replication plugin as a base for other replication
    plugins more API needs to be exposed.
    
    All classes bound in ReplicationModule are made public with protected
    constructors to enable extendability.
    
    Also part of configuration API is exposed for supporting different
    configuration sources then $site_path/replication.config eg. dynamically
    load from 'master' server.
    
    Last thing is to enable consumers to modify SSH session eg. modify
    credentials factory and/or set know_hosts repository. For this purpose
    ReplicationSshSessionFactory interface was added.
    
    Change-Id: I2e26fc976b04bd1ace5ddd695ef558c6d66a4716
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

--- src/main/java/com/googlesource/gerrit/plugins/replication/CredentialsFactory.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/CredentialsFactory.java
@@ -13,7 +13,7 @@
 // limitations under the License.
 package com.googlesource.gerrit.plugins.replication;
 
-interface CredentialsFactory {
+public interface CredentialsFactory {
 
   SecureCredentialsProvider create(String remoteName);
 
