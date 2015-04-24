f0a17552ca11fafb7b1db4332fb01185ba7a49b9
Liu Forest <forest.liu@sonymobile.com>
2015-04-24 16:20:42 +0800

    Add a command to list replication targets
    Usage:
    ssh -p <SSH_PORT> [USER]@<SSH_HOST> replication list
      [--remote <PATTERN>]
      [--detail]
      [--json]
    
    Change-Id: Iccb2d12a9d694d343949ef30b0004d6cbb3aa347

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ListCommand.java
@@ -0,0 +1,116 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import com.google.gerrit.common.data.GlobalCapability;
+import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gerrit.sshd.CommandMetaData;
+import com.google.gerrit.sshd.SshCommand;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.common.base.Strings;
+import com.google.inject.Inject;
+
+import com.googlesource.gerrit.plugins.replication.ReplicationConfig.FilterType;
+
+import org.kohsuke.args4j.Option;
+
+import java.util.List;
+
+@RequiresCapability(GlobalCapability.ADMINISTRATE_SERVER)
+@CommandMetaData(name = "list", description = "List specific remote destinations information")
+final class ListCommand extends SshCommand {
+  private static final char endl = '\n';
+
+  @Option(name = "--remote", metaVar = "PATTERN", usage = "pattern to match remote name on")
+  private String remote;
+
+  @Option(name = "--detail", usage = "print remote destination detail information")
+  private boolean detail;
+
+  @Option(name = "--json", usage = "output in json format")
+  private boolean json;
+
+  @Inject
+  private ReplicationConfig config;
+
+  @Override
+  protected void run() {
+    List<Destination> dest = config.getDestinations(FilterType.ALL);
+    for (Destination d : dest) {
+      if (matches(d.getRemoteConfig().getName())) {
+        printRemote(d, detail);
+      }
+    }
+  }
+
+  private boolean matches(String name) {
+    if ( Strings.isNullOrEmpty(remote)) {
+      return true;
+    }
+
+    if (name.contains(remote) || name.matches(remote)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private void addProperty(JsonObject obj, String key, String[] values) {
+    if (values.length > 0) {
+      JsonArray list = new JsonArray();
+      for (String v : values) {
+        list.add(new JsonPrimitive(v));
+      }
+      obj.add(key, list);
+    }
+  }
+
+  private void printRemote(Destination d, boolean detail) {
+    if (json) {
+      JsonObject obj = new JsonObject();
+      obj.addProperty("remote", d.getRemoteConfig().getName());
+      addProperty(obj, "Url", d.getUrls());
+      if (detail) {
+        addProperty(obj, "AdminUrl", d.getAdminUrls());
+        addProperty(obj, "AuthGroup", d.getAuthGroupNames());
+        addProperty(obj, "Project", d.getProjects());
+      }
+      stdout.print(obj.toString() + endl);
+    } else {
+      StringBuilder out = new StringBuilder();
+      out.append("Remote: " + d.getRemoteConfig().getName() + endl);
+      for (String url : d.getUrls()) {
+        out.append("Url: " + url + endl);
+      }
+
+      if (detail) {
+        for (String adminUrl : d.getAdminUrls()) {
+          out.append("AdminUrl: " + adminUrl + endl);
+        }
+
+        for (String authGroup : d.getAuthGroupNames()) {
+          out.append("AuthGroup: " + authGroup + endl);
+        }
+
+        for (String project : d.getProjects()) {
+          out.append("Project: " + project + endl);
+        }
+      }
+      stdout.print(out.toString() + endl);
+    }
+  }
+}
