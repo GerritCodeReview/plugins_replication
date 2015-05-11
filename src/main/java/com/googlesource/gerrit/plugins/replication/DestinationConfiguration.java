664c5e446cfe310ec2a9c2b651004f9fb20266dd
Dariusz Luksza <dariusz@luksza.org>
2015-05-11 13:45:12 +0200

    Extract replication configuration from Destination class
    Split configuration from thread pool management. This allows dynamically
    change configuration options eg. disable/enable project configuration.
    
    Change-Id: If46ff42e86a032701deb1607d4ed88d77f0310b9
    Signed-off-by: Dariusz Luksza <dariusz@luksza.org>

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/DestinationConfiguration.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2015 CollabNet, Inc. All rights reserved.
+ * http://www.collab.net
+ */
+
+package com.googlesource.gerrit.plugins.replication;
+
+public interface DestinationConfiguration {
+
+  int getDelay();
+  int getRetryDelay();
+  int getPoolThreads();
+  int getLockErrorMaxRetries();
+  String[] getUrls();
+  String[] getProjects();
+  String[] getAdminUrls();
+  String[] getAuthGroupNames();
+  String getRemoteNameStyle();
+  boolean replicatePermissions();
+  boolean createMissingRepos();
+  boolean replicateProjectDeletions();
+}
