28263d56fe4913f9920aa9bdec06c5c9a0d60c6a
Jacek Centkowski <geminica.programs@gmail.com>
2015-09-18 10:25:57 +0200

    Handle kill command in replication tasks also while running
    Issue description:
    When replication tasks is scheduled and it gets killed with ssh kill
    command further replication attempts get ignored and as a result
    replication to particular mirror is no longer possible (Gerrit needs
    to be restarted or plugin reloaded).
    
    Solution:
    Step1: Derive PushOne from CanceledWhileRunning interface so that
    it gets informed that it was canceled while operation was
    in progress (which results in any resource operation failure).
    
    Step2: check in resource failure catch if it occurred while task
    was canceled. If that is the case don't reschedule.
    
    Change-Id: I4882fe1334efedf060a5ff5fef89d9d33c88d75c
    Depends-On: I2e44407f68ebcf0aa92e1e9e30e10f26feed7182
    Signed-off-by: Jacek Centkowski <geminica.programs@gmail.com>

--- src/main/java/com/googlesource/gerrit/plugins/replication/Destination.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/Destination.java
@@ -267,6 +267,13 @@ public class Destination {
     }
   }
 
+  void pushWasCancelled(PushOne pushOp) {
+    synchronized (stateLock) {
+      URIish uri = pushOp.getURI();
+      pending.remove(uri);
+    }
+  }
+
   /**
    * It schedules again a PushOp instance.
    * <p>
@@ -332,8 +339,7 @@ public class Destination {
           // when notifying it is starting (with pending lock protection),
           // it will see it was canceled and then it will do nothing with
           // pending list and it will not execute its run implementation.
-
-          pendingPushOp.cancel();
+          pendingPushOp.cancelledByReplication();
           pending.remove(uri);
 
           pushOp.addRefs(pendingPushOp.getRefs());
@@ -366,7 +372,7 @@ public class Destination {
 
   boolean requestRunway(PushOne op) {
     synchronized (stateLock) {
-      if (op.wasCanceled()) {
+      if (op.wasCancelled()) {
         return false;
       }
       pending.remove(op.getURI());
@@ -378,9 +384,9 @@ public class Destination {
     return true;
   }
 
-  void notifyFinished(PushOne op) {
+  PushOne notifyFinished(PushOne op) {
     synchronized (stateLock) {
-      inFlight.remove(op.getURI());
+      return inFlight.remove(op.getURI());
     }
   }
 
