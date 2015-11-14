ea41e2cb3ff3db764e77fafdae2511f8bfc5f8d4
David Pursehouse <david.pursehouse@sonymobile.com>
2015-11-13 23:08:02 -0800

    Add metrics for replication delay and retries
    Change-Id: I22c55de32636bcf19f5723477c603e6450b3479b

--- src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationMetrics.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationMetrics.java
@@ -16,6 +16,7 @@ package com.googlesource.gerrit.plugins.replication;
 
 import com.google.gerrit.metrics.Description;
 import com.google.gerrit.metrics.Field;
+import com.google.gerrit.metrics.Histogram1;
 import com.google.gerrit.metrics.MetricMaker;
 import com.google.gerrit.metrics.Timer1;
 import com.google.inject.Inject;
@@ -24,15 +25,33 @@ import com.google.inject.Singleton;
 @Singleton
 public class ReplicationMetrics {
   Timer1<String> executionTime;
+  Histogram1<String> executionDelay;
+  Histogram1<String> executionRetries;
 
   @Inject
   ReplicationMetrics(MetricMaker metricMaker) {
+    Field<String> DEST_FIELD = Field.ofString("destination");
+
     executionTime = metricMaker.newTimer(
         "replication_latency",
         new Description("Time spent pushing to remote destination.")
           .setCumulative()
           .setUnit(Description.Units.MILLISECONDS),
-        Field.ofString("destination"));
+        DEST_FIELD);
+
+    executionDelay = metricMaker.newHistogram(
+        "replication_delay",
+        new Description("Time spent waiting before pushing to remote destination")
+          .setCumulative()
+          .setUnit(Description.Units.MILLISECONDS),
+        DEST_FIELD);
+
+    executionRetries = metricMaker.newHistogram(
+        "replication_retries",
+        new Description("Number of retries when pushing to remote destination")
+          .setCumulative()
+          .setUnit("retries"),
+        DEST_FIELD);
   }
 
   /**
@@ -42,6 +61,17 @@ public class ReplicationMetrics {
    */
   Timer1.Context start(String name) {
     return executionTime.start(name);
+  }
+
+  /**
+   * Record the replication delay and retry metrics for a destination.
+   * @param name the destination name.
+   * @param delay replication delay in milliseconds.
+   * @param retries number of retries.
+   */
+  void record(String name, long delay, long retries) {
+    executionDelay.record(name, delay);
+    executionRetries.record(name, retries);
   }
 
 }
