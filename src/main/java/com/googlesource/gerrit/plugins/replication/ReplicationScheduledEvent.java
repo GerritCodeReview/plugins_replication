0c781374c9f231ddc9eb34d4a5ab63ddab98f5d8
Eryk Szymanski <eryksz@gmail.com>
2016-03-15 14:47:20 +0100

    Add ReplicationScheduledEvent
    Assure that event is posted for every ref
    Covers the corner case when the ref was added to already scheduled push
    
    Change-Id: I175940f8d4c6b9164c41bbec8c52d221ec30001b
    Signed-off-by: Eryk Szymanski <eryksz@gmail.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationScheduledEvent.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.client.Project.NameKey;
+import com.google.gerrit.server.events.RefEvent;
+
+public class ReplicationScheduledEvent extends RefEvent {
+  public static final String TYPE = "ref-replication-scheduled";
+
+  public final String project;
+  public final String ref;
+  public final String targetNode;
+
+  public ReplicationScheduledEvent(String project, String ref,
+      String targetNode) {
+    super(TYPE);
+    this.project = project;
+    this.ref = ref;
+    this.targetNode = targetNode;
+  }
+
+  @Override
+  public String getRefName() {
+    return ref;
+  }
+
+  @Override
+  public NameKey getProjectNameKey() {
+    return new Project.NameKey(project);
+  }
+}
