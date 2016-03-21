8419e92cb014c1fa5a1f1757e187bf880f99b476
Dave Borowitz <dborowitz@google.com>
2016-03-21 12:04:48 -0400

    Remove usages of Throwables#propagate
    Guava intends to deprecate these, and recommends the 2-line inlining
    of the method instead.
    
    Change-Id: I74a45915effaae2f29090f27b9b258e2c7c9a7d0

--- src/main/java/com/googlesource/gerrit/plugins/replication/Destination.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/Destination.java
@@ -262,7 +262,8 @@ public class Destination {
       stateLog.error(String.format("source project %s not available", project),
           err, states);
     } catch (Exception e) {
-      throw Throwables.propagate(e);
+      Throwables.propagateIfPossible(e);
+      throw new RuntimeException(e);
     }
     return false;
   }
