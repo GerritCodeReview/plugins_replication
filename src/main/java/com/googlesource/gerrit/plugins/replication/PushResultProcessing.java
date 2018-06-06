da3980a431ba9138c9af91b6c3d9eaed4f4bc781
Edwin Kempin <ekempin@google.com>
2018-06-06 10:18:30 +0200

    Make loggers private
    Loggers shouldn't be shared between classes and hence they should be
    private.
    
    Change-Id: Iadd54c73455665b53cf9b58918b2e8c5cf19357e
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- src/main/java/com/googlesource/gerrit/plugins/replication/PushResultProcessing.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/PushResultProcessing.java
@@ -158,7 +158,7 @@ public abstract class PushResultProcessing {
   }
 
   public static class GitUpdateProcessing extends PushResultProcessing {
-    static final Logger log = LoggerFactory.getLogger(GitUpdateProcessing.class);
+    private static final Logger log = LoggerFactory.getLogger(GitUpdateProcessing.class);
 
     private final EventDispatcher dispatcher;
 
