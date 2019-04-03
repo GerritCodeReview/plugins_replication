ba1b0decc15bf4a07bca6c4c8edd947b83a2fe18
Luca Milanesio <luca.milanesio@gmail.com>
2019-04-03 16:35:12 +0100

    Break Config and Destination logic into separate classes
    Parsing the configuration and creating the JGit destinations are two
    different concerns: refactor the current configuration monolith into
    two different classes, introducing the Destinations class.
    
    Change-Id: Ieebae7963c0bd4bde89a4e21cc9c661e872c04e3

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/DestinationsCollection.java
@@ -0,0 +1,167 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import static java.util.stream.Collectors.toList;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.server.git.WorkQueue;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.replication.ReplicationConfig.FilterType;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Predicate;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.RemoteConfig;
+import org.eclipse.jgit.transport.URIish;
+
+/** Collection of Git repositories destinations for replication. */
+public class DestinationsCollection {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final ReplicationConfig replicationConfig;
+  private final Destination.Factory destinationFactory;
+  private List<Destination> allDestinations = Collections.emptyList();
+
+  @Inject
+  DestinationsCollection(
+      ReplicationConfig replicationConfig, Destination.Factory destinationFactory) {
+    this.replicationConfig = replicationConfig;
+    this.destinationFactory = destinationFactory;
+  }
+
+  /**
+   * Get all destinations matching the specified type.
+   *
+   * @param filterType type of destination.
+   * @return list of destinations matching the specified filter type.
+   */
+  public List<Destination> getAll(FilterType filterType) {
+    if (replicationConfig.reloadIfNeeded()) {
+      try {
+        load();
+      } catch (ConfigInvalidException e) {
+        logger.atWarning().withCause(e).log("Unable to load new destinations");
+      }
+    }
+
+    Predicate<? super Destination> filter;
+    switch (filterType) {
+      case PROJECT_CREATION:
+        filter = dest -> dest.isCreateMissingRepos();
+        break;
+      case PROJECT_DELETION:
+        filter = dest -> dest.isReplicateProjectDeletions();
+        break;
+      case ALL:
+      default:
+        filter = dest -> true;
+        break;
+    }
+    return allDestinations.stream().filter(Objects::nonNull).filter(filter).collect(toList());
+  }
+
+  /** @return true if there are no destinations. */
+  public boolean isEmpty() {
+    return allDestinations.isEmpty();
+  }
+
+  /**
+   * Start replication on all destinations.
+   *
+   * @param workQueue replication queue.
+   * @throws ConfigInvalidException
+   */
+  public void startup(WorkQueue workQueue) throws ConfigInvalidException {
+    load();
+    for (Destination destination : allDestinations) {
+      destination.start(workQueue);
+    }
+  }
+
+  /**
+   * Shutdown replication on all destinations.
+   *
+   * @return number of pending replication events that have been discarded.
+   */
+  public int shutdown() {
+    int discarded = 0;
+    for (Destination destination : allDestinations) {
+      discarded += destination.shutdown();
+    }
+    return discarded;
+  }
+
+  private void load() throws ConfigInvalidException {
+    ImmutableList.Builder<Destination> dest = ImmutableList.builder();
+    for (RemoteConfig c : allRemotes()) {
+      if (c.getURIs().isEmpty()) {
+        continue;
+      }
+
+      // If destination for push is not set assume equal to source.
+      for (RefSpec ref : c.getPushRefSpecs()) {
+        if (ref.getDestination() == null) {
+          ref.setDestination(ref.getSource());
+        }
+      }
+
+      if (c.getPushRefSpecs().isEmpty()) {
+        c.addPushRefSpec(
+            new RefSpec()
+                .setSourceDestination("refs/*", "refs/*")
+                .setForceUpdate(replicationConfig.isDefaultForceUpdate()));
+      }
+
+      Destination destination =
+          destinationFactory.create(new DestinationConfiguration(c, replicationConfig.getConfig()));
+
+      if (!destination.isSingleProjectMatch()) {
+        for (URIish u : c.getURIs()) {
+          if (u.getPath() == null || !u.getPath().contains("${name}")) {
+            throw new ConfigInvalidException(
+                String.format("remote.%s.url \"%s\" lacks ${name} placeholder", c.getName(), u));
+          }
+        }
+      }
+
+      dest.add(destination);
+    }
+
+    this.allDestinations = dest.build();
+    logger.atInfo().log("%d replication destinations loaded", allDestinations.size());
+  }
+
+  private List<RemoteConfig> allRemotes() throws ConfigInvalidException {
+    Config config = replicationConfig.getConfig();
+    Set<String> names = config.getSubsections("remote");
+    List<RemoteConfig> result = Lists.newArrayListWithCapacity(names.size());
+    for (String name : names) {
+      try {
+        result.add(new RemoteConfig(config, name));
+      } catch (URISyntaxException e) {
+        throw new ConfigInvalidException(String.format("remote %s has invalid URL", name));
+      }
+    }
+    return result;
+  }
+}
--- src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationFileBasedConfig.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationFileBasedConfig.java
@@ -13,37 +13,23 @@
 // limitations under the License.
 package com.googlesource.gerrit.plugins.replication;
 
-import static java.util.stream.Collectors.toList;
-
 import com.google.common.base.Strings;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.extensions.annotations.PluginData;
 import com.google.gerrit.server.config.SitePaths;
-import com.google.gerrit.server.git.WorkQueue;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
-import java.util.function.Predicate;
 import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.storage.file.FileBasedConfig;
-import org.eclipse.jgit.transport.RefSpec;
-import org.eclipse.jgit.transport.RemoteConfig;
-import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.util.FS;
 
 @Singleton
 public class ReplicationFileBasedConfig implements ReplicationConfig {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
-  private List<Destination> destinations;
   private final SitePaths site;
   private Path cfgPath;
   private boolean replicateAllOnPluginStart;
@@ -52,49 +38,21 @@ public class ReplicationFileBasedConfig implements ReplicationConfig {
   private final Path pluginDataDir;
 
   @Inject
-  public ReplicationFileBasedConfig(
-      SitePaths site, Destination.Factory destinationFactory, @PluginData Path pluginDataDir)
+  public ReplicationFileBasedConfig(SitePaths site, @PluginData Path pluginDataDir)
       throws ConfigInvalidException, IOException {
     this.site = site;
     this.cfgPath = site.etc_dir.resolve("replication.config");
     this.config = new FileBasedConfig(cfgPath.toFile(), FS.DETECTED);
-    this.destinations = allDestinations(destinationFactory);
     this.pluginDataDir = pluginDataDir;
+    load();
   }
 
-  /*
-   * (non-Javadoc)
-   * @see
-   * com.googlesource.gerrit.plugins.replication.ReplicationConfig#getDestinations
-   * (com.googlesource.gerrit.plugins.replication.ReplicationConfig.FilterType)
-   */
-  @Override
-  public List<Destination> getDestinations(FilterType filterType) {
-    Predicate<? super Destination> filter;
-    switch (filterType) {
-      case PROJECT_CREATION:
-        filter = dest -> dest.isCreateMissingRepos();
-        break;
-      case PROJECT_DELETION:
-        filter = dest -> dest.isReplicateProjectDeletions();
-        break;
-      case ALL:
-      default:
-        filter = dest -> true;
-        break;
-    }
-    return destinations.stream().filter(Objects::nonNull).filter(filter).collect(toList());
-  }
-
-  private List<Destination> allDestinations(Destination.Factory destinationFactory)
-      throws ConfigInvalidException, IOException {
+  private void load() throws ConfigInvalidException, IOException {
     if (!config.getFile().exists()) {
       logger.atWarning().log("Config file %s does not exist; not replicating", config.getFile());
-      return Collections.emptyList();
     }
     if (config.getFile().length() == 0) {
       logger.atInfo().log("Config file %s is empty; not replicating", config.getFile());
-      return Collections.emptyList();
     }
 
     try {
@@ -110,43 +68,6 @@ public class ReplicationFileBasedConfig implements ReplicationConfig {
     replicateAllOnPluginStart = config.getBoolean("gerrit", "replicateOnStartup", false);
 
     defaultForceUpdate = config.getBoolean("gerrit", "defaultForceUpdate", false);
-
-    ImmutableList.Builder<Destination> dest = ImmutableList.builder();
-    for (RemoteConfig c : allRemotes(config)) {
-      if (c.getURIs().isEmpty()) {
-        continue;
-      }
-
-      // If destination for push is not set assume equal to source.
-      for (RefSpec ref : c.getPushRefSpecs()) {
-        if (ref.getDestination() == null) {
-          ref.setDestination(ref.getSource());
-        }
-      }
-
-      if (c.getPushRefSpecs().isEmpty()) {
-        c.addPushRefSpec(
-            new RefSpec()
-                .setSourceDestination("refs/*", "refs/*")
-                .setForceUpdate(defaultForceUpdate));
-      }
-
-      Destination destination = destinationFactory.create(new DestinationConfiguration(c, config));
-
-      if (!destination.isSingleProjectMatch()) {
-        for (URIish u : c.getURIs()) {
-          if (u.getPath() == null || !u.getPath().contains("${name}")) {
-            throw new ConfigInvalidException(
-                String.format(
-                    "remote.%s.url \"%s\" lacks ${name} placeholder in %s",
-                    c.getName(), u, config.getFile()));
-          }
-        }
-      }
-
-      dest.add(destination);
-    }
-    return dest.build();
   }
 
   /* (non-Javadoc)
@@ -165,28 +86,6 @@ public class ReplicationFileBasedConfig implements ReplicationConfig {
     return defaultForceUpdate;
   }
 
-  private static List<RemoteConfig> allRemotes(FileBasedConfig cfg) throws ConfigInvalidException {
-    Set<String> names = cfg.getSubsections("remote");
-    List<RemoteConfig> result = Lists.newArrayListWithCapacity(names.size());
-    for (String name : names) {
-      try {
-        result.add(new RemoteConfig(cfg, name));
-      } catch (URISyntaxException e) {
-        throw new ConfigInvalidException(
-            String.format("remote %s has invalid URL in %s", name, cfg.getFile()));
-      }
-    }
-    return result;
-  }
-
-  /* (non-Javadoc)
-   * @see com.googlesource.gerrit.plugins.replication.ReplicationConfig#isEmpty()
-   */
-  @Override
-  public boolean isEmpty() {
-    return destinations.isEmpty();
-  }
-
   @Override
   public Path getEventsDirectory() {
     String eventsDirectory = config.getString("replication", null, "eventsDirectory");
@@ -201,22 +100,12 @@ public class ReplicationFileBasedConfig implements ReplicationConfig {
   }
 
   @Override
-  public int shutdown() {
-    int discarded = 0;
-    for (Destination cfg : destinations) {
-      discarded += cfg.shutdown();
-    }
-    return discarded;
-  }
-
-  FileBasedConfig getConfig() {
+  public Config getConfig() {
     return config;
   }
 
   @Override
-  public void startup(WorkQueue workQueue) {
-    for (Destination cfg : destinations) {
-      cfg.start(workQueue);
-    }
+  public boolean reloadIfNeeded() {
+    return false;
   }
 }
