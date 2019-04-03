4ec7f84642d5bd395376a66f55875fbd636e6f3e
Luca Milanesio <luca.milanesio@gmail.com>
2019-04-03 14:13:02 +0100

    Remove unneeded RemoteSiteUser.Factory
    The RemoteSiteUser is a simple wrapper of a user with a set of groups
    and does not inject any other object. Using Guice injection is thus
    a redundant level of indirection.
    
    Change-Id: Ic1a436b8829a873e53d6f0be2fff0e405dbf54c2

--- src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationModule.java
@@ -58,7 +58,6 @@ class ReplicationModule extends AbstractModule {
         .to(StartReplicationCapability.class);
 
     install(new FactoryModuleBuilder().build(PushAll.Factory.class));
-    install(new FactoryModuleBuilder().build(RemoteSiteUser.Factory.class));
     install(new FactoryModuleBuilder().build(ReplicationState.Factory.class));
 
     bind(ReplicationConfig.class).to(AutoReloadConfigDecorator.class);
