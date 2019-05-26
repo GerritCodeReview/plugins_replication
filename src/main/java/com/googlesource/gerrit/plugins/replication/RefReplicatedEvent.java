f9049a7698f3c3a4b89844de3338c251632d14a9
David Ostrovsky <david@ostrovsky.org>
2019-05-26 14:02:42 +0200

    Rename reviewdb package to entities
    Change-Id: I402c3ff9cd2064fcd3a2cfc2249bf00d87104327

--- src/main/java/com/googlesource/gerrit/plugins/replication/RefReplicatedEvent.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/RefReplicatedEvent.java
@@ -14,7 +14,7 @@
 
 package com.googlesource.gerrit.plugins.replication;
 
-import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.entities.Project;
 import com.google.gerrit.server.events.RefEvent;
 import com.googlesource.gerrit.plugins.replication.ReplicationState.RefPushResult;
 import java.util.Objects;
