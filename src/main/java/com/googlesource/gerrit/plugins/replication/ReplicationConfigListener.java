4130a45315ae399b32912ef92180bd3c0e039bec
Luca Milanesio <luca.milanesio@gmail.com>
2019-06-06 21:11:12 +0100

    Split responsibilities of the auto reload config
    Reduce the complexity of the AutoReloadConfigDecorator
    by decomposing it into three parts:
    
    1. Background thread for detecting and reloading the config.
       AutoReloadRunnable is only aware of what needs to be done to reload
       a configuration safely and notify whoever is bound to react on
       configuration reloads.
    
    2. Listener interface to notify when the configuration has changed
       ReplicationConfigListener allows performing actions before and after
       the configuration reload.
    
    3. Decorator of the ReplicationFileBasedConfig that orchestrate reload
       AutoReloadConfigDecorator has a lot fewer responsibilities and is
       limited to delegate the config and destinations interface methods
       and acting as configuration listener.
    
    Allows to put back the logic of the file-based config into the
    respective FileBasedReplicationConfig and abstract the concept of the
    last modification in a generic version string.
    
    Change-Id: Iaee9b72801bbe3ddac5113df6c53676934687aab

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationConfigListener.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import org.eclipse.jgit.errors.ConfigInvalidException;
+
+/** Listener of the configuration events. */
+public interface ReplicationConfigListener {
+
+  /**
+   * Invoked just before replication.config is about to be loaded.
+   *
+   * @param oldConfig old replication config
+   */
+  void beforeLoad(ReplicationFileBasedConfig oldConfig);
+
+  /**
+   * Invoked just after replication.config is loaded into memory.
+   *
+   * @param newConfig new replication config
+   * @throws ConfigInvalidException if the loaded configuration is not valid
+   */
+  void afterLoad(ReplicationFileBasedConfig newConfig) throws ConfigInvalidException;
+}
