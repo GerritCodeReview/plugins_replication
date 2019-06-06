32d755ac6a4af46d408256cefae5c6338f7c49e4
Luca Milanesio <luca.milanesio@gmail.com>
2019-06-06 21:11:12 +0100

    Split responsibilities of the auto reload config
    Reduce the complexity of the AutoReloadConfigDecorator
    by decomposing it into two parts:
    
    1. Background thread for detecting and reloading the config.
       AutoReloadRunnable is only aware of what needs to be done to reload
       a configuration safely and notify whoever is bound to react on
       configuration reloads.
    
    2. Decorator of the ReplicationFileBasedConfig that orchestrate reload.
       AutoReloadConfigDecorator has a lot fewer responsibilities and is
       limited to delegate the config and destinations interface methods
       and acting as configuration listener.
    
    The propagation of the new configuration to the other objects
    is done through a Guava's EventBus.
    
    Allows to put back the logic of the file-based config into the
    respective FileBasedReplicationConfig and abstract the concept of the
    last modification in a generic version string.
    
    NOTE: The validation of the new configuration is left out of scope
          of this change and is implemented separately.
    
    Feature: Issue 11425
    Change-Id: Iaee9b72801bbe3ddac5113df6c53676934687aab

--- src/test/java/com/googlesource/gerrit/plugins/replication/ReplicationIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/replication/ReplicationIT.java
@@ -29,6 +29,7 @@ import com.google.gerrit.extensions.api.projects.BranchInput;
 import com.google.gerrit.extensions.common.ProjectInfo;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Key;
 import com.googlesource.gerrit.plugins.replication.ReplicationTasksStorage.ReplicateRefUpdate;
@@ -53,7 +54,7 @@ import org.junit.Test;
 @UseLocalDisk
 @TestPlugin(
     name = "replication",
-    sysModule = "com.googlesource.gerrit.plugins.replication.ReplicationModule")
+    sysModule = "com.googlesource.gerrit.plugins.replication.ReplicationIT$TestReplicationModule")
 public class ReplicationIT extends LightweightPluginDaemonTest {
   private static final Optional<String> ALL_PROJECTS = Optional.empty();
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
@@ -67,6 +68,24 @@ public class ReplicationIT extends LightweightPluginDaemonTest {
   private Path storagePath;
   private FileBasedConfig config;
   private ReplicationTasksStorage tasksStorage;
+
+  public static class TestReplicationModule extends AbstractModule {
+    @Inject private SitePaths sitePaths;
+
+    @Override
+    protected void configure() {
+      FileBasedConfig cfg =
+          new FileBasedConfig(
+              sitePaths.etc_dir.resolve("replication.config").toFile(), FS.DETECTED);
+      cfg.setBoolean("gerrit", null, "autoReload", true);
+      try {
+        cfg.save();
+      } catch (IOException e) {
+        throw new RuntimeException("Cannot save config file: " + cfg);
+      }
+      install(new ReplicationModule(sitePaths));
+    }
+  }
 
   @Override
   public void setUpTestPlugin() throws Exception {
