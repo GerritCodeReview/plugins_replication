38c8023d61ecf33dcb7d1c224eeb449a843e0ae2
Luca Milanesio <luca.milanesio@gmail.com>
2019-06-06 21:11:12 +0100

    Split responsibilities of the auto reload config
    Reduce the complexity of the AutoReloadConfigDecorator
    by decomposing it into two parts:
    
    1. Background thread for detecting and reloading the config.
       AutoReloadRunnable is only aware of what needs to be done to reload
       a configuration safely and notify whoever is bound to react on
       configuration reloads.
    
    2. Decorator of the ReplicationFileBasedConfig that orchestrate reload.
       AutoReloadConfigDecorator has a lot fewer responsibilities and is
       limited to delegate the config and destinations interface methods
       and acting as configuration listener.
    
    The propagation of the new configuration to the other objects
    is done through a Guava's EventBus.
    
    Allows to put back the logic of the file-based config into the
    respective FileBasedReplicationConfig and abstract the concept of the
    last modification in a generic version string.
    
    NOTE: The validation of the new configuration is left out of scope
          of this change and is implemented separately.
    
    Feature: Issue 11425
    Change-Id: Iaee9b72801bbe3ddac5113df6c53676934687aab

--- src/main/java/com/googlesource/gerrit/plugins/replication/AutoReloadConfigDecorator.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/AutoReloadConfigDecorator.java
@@ -15,24 +15,21 @@ package com.googlesource.gerrit.plugins.replication;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Multimap;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import com.google.common.flogger.FluentLogger;
-import com.google.gerrit.common.FileUtil;
-import com.google.gerrit.extensions.annotations.PluginData;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.reviewdb.client.Project;
-import com.google.gerrit.server.config.SitePaths;
 import com.google.gerrit.server.git.WorkQueue;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.transport.URIish;
 
 @Singleton
@@ -42,46 +39,28 @@ public class AutoReloadConfigDecorator implements ReplicationConfig, Replication
   private static final long RELOAD_INTERVAL = 60;
 
   private ReplicationFileBasedConfig currentConfig;
-  private long currentConfigTs;
-  private long lastFailedConfigTs;
 
-  private final SitePaths site;
-  private final Destination.Factory destinationFactory;
-  private final Path pluginDataDir;
-  // Use Provider<> instead of injecting the ReplicationQueue because of circular dependency with
+  // Use Provider<> instead of injecting the ReplicationQueue because of
+  // circular dependency with
   // ReplicationConfig
   private final Provider<ReplicationQueue> replicationQueue;
   private final ScheduledExecutorService autoReloadExecutor;
   private ScheduledFuture<?> autoReloadRunnable;
+  private AutoReloadRunnable reloadRunner;
 
   @Inject
   public AutoReloadConfigDecorator(
-      SitePaths site,
-      Destination.Factory destinationFactory,
       Provider<ReplicationQueue> replicationQueue,
-      @PluginData Path pluginDataDir,
       @PluginName String pluginName,
-      WorkQueue workQueue)
-      throws ConfigInvalidException, IOException {
-    this.site = site;
-    this.destinationFactory = destinationFactory;
-    this.pluginDataDir = pluginDataDir;
-    this.currentConfig = loadConfig();
-    this.currentConfigTs = getLastModified(currentConfig);
+      WorkQueue workQueue,
+      ReplicationFileBasedConfig replicationConfig,
+      AutoReloadRunnable reloadRunner,
+      EventBus eventBus) {
+    this.currentConfig = replicationConfig;
     this.replicationQueue = replicationQueue;
     this.autoReloadExecutor = workQueue.createQueue(1, pluginName + "_auto-reload-config");
-  }
-
-  private static long getLastModified(ReplicationFileBasedConfig cfg) {
-    return FileUtil.lastModified(cfg.getCfgPath());
-  }
-
-  private ReplicationFileBasedConfig loadConfig() throws ConfigInvalidException, IOException {
-    return new ReplicationFileBasedConfig(site, destinationFactory, pluginDataDir);
-  }
-
-  private synchronized boolean isAutoReload() {
-    return currentConfig.getConfig().getBoolean("gerrit", "autoReload", false);
+    this.reloadRunner = reloadRunner;
+    eventBus.register(this);
   }
 
   @Override
@@ -95,43 +74,9 @@ public class AutoReloadConfigDecorator implements ReplicationConfig, Replication
     return currentConfig.getURIs(remoteName, projectName, filterType);
   }
 
-  private synchronized void reloadIfNeeded() {
-    reload(false);
-  }
-
   @VisibleForTesting
   public void forceReload() {
-    reload(true);
-  }
-
-  private void reload(boolean force) {
-    if (force || isAutoReload()) {
-      ReplicationQueue queue = replicationQueue.get();
-
-      long lastModified = getLastModified(currentConfig);
-      try {
-        if (force
-            || (lastModified > currentConfigTs
-                && lastModified > lastFailedConfigTs
-                && queue.isRunning()
-                && !queue.isReplaying())) {
-          queue.stop();
-          currentConfig = loadConfig();
-          currentConfigTs = lastModified;
-          lastFailedConfigTs = 0;
-          logger.atInfo().log(
-              "Configuration reloaded: %d destinations",
-              currentConfig.getAll(FilterType.ALL).size());
-        }
-      } catch (Exception e) {
-        logger.atSevere().withCause(e).log(
-            "Cannot reload replication configuration: keeping existing settings");
-        lastFailedConfigTs = lastModified;
-        return;
-      } finally {
-        queue.start();
-      }
-    }
+    reloadRunner.forceReload();
   }
 
   @Override
@@ -173,7 +118,24 @@ public class AutoReloadConfigDecorator implements ReplicationConfig, Replication
     currentConfig.startup(workQueue);
     autoReloadRunnable =
         autoReloadExecutor.scheduleAtFixedRate(
-            this::reloadIfNeeded, RELOAD_DELAY, RELOAD_INTERVAL, TimeUnit.SECONDS);
+            reloadRunner, RELOAD_DELAY, RELOAD_INTERVAL, TimeUnit.SECONDS);
+  }
+
+  @Override
+  public String getVersion() {
+    return currentConfig.getVersion();
+  }
+
+  @Subscribe
+  public void onReload(ReplicationFileBasedConfig newConfig) {
+    try {
+      replicationQueue.get().stop();
+      currentConfig = newConfig;
+      logger.atInfo().log(
+          "Configuration reloaded: %d destinations", newConfig.getAll(FilterType.ALL).size());
+    } finally {
+      replicationQueue.get().start();
+    }
   }
 
   @Override
