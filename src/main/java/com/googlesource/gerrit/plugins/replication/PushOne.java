c3e8d153316035a0c24d8383be1cb2390752ab82
Saša Živkov <sasa.zivkov@sap.com>
2019-06-21 10:51:34 +0200

    Make sure to always remove in-flight pushes
    When a push is finished successfully or fails due to an error we have to
    remove it from the map of in-flight pushes. Although this was done from
    a finally block, the same block performed a Repository.close() operation
    prior to the in-flight push removal. If the Repository.close() threw an
    exception the pool.notifyFinished() wouldn't be called and we would end
    up with a zombie in-flight push.
    
    Bug: Issue 10852
    Change-Id: I8d5918d5271ba74ce12153f054503adaef155c5c
    (cherry picked from commit 6fcdcf98fe61e8c6258fb40967f0cff6b8c38fd4)

--- src/main/java/com/googlesource/gerrit/plugins/replication/PushOne.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/PushOne.java
@@ -379,10 +379,10 @@ class PushOne implements ProjectRunnable, CanceledWhileRunning {
     } catch (PermissionBackendException | RuntimeException | Error e) {
       stateLog.error("Unexpected error during replication to " + uri, e, getStatesAsArray());
     } finally {
+      pool.notifyFinished(this);
       if (git != null) {
         git.close();
       }
-      pool.notifyFinished(this);
     }
   }
 
