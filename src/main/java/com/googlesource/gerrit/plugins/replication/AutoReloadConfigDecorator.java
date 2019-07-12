def6372ac3387368e3d1096f13a48a0de1f88329
Luca Milanesio <luca.milanesio@gmail.com>
2019-07-12 22:41:33 +0100

    Fix granularity of persistent replication events
    Align the granularity of persistent events on disk with the replication events
    queued or in-flight.
    
    Fixes a recurring issue where persistent events were missing or left orphan on the
    filesystem due to the different granularity of the incoming ref-update events and
    the corresponding git push operation queued and executed.
    
    Bug: Issue 11172
    Change-Id: I02d8cda0a124e8e3d2b9bb01b7d44f98ba717fcd

--- src/main/java/com/googlesource/gerrit/plugins/replication/AutoReloadConfigDecorator.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/AutoReloadConfigDecorator.java
@@ -144,11 +144,6 @@ public class AutoReloadConfigDecorator implements ReplicationConfig {
   }
 
   @Override
-  public Path getEventsDirectory() {
-    return currentConfig.getEventsDirectory();
-  }
-
-  @Override
   public synchronized int shutdown() {
     if (autoReloadRunnable != null) {
       autoReloadRunnable.cancel(false);
