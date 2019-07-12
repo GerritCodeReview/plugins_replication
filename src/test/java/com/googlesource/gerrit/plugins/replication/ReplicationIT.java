746ec592b2ae927d8d7b4eb396c85b7590d0ee81
Luca Milanesio <luca.milanesio@gmail.com>
2019-07-12 22:41:33 +0100

    Store replication tasks instead of ref-update events
    Fix the granularity of persistent events on disk by storing
    replication tasks rather than ref-update events.
    
    Ref-updates are triggering the replication tasks,
    however it is tricky to perform reference counting on the associated
    tasks reliably, due to all the different conditions of where a
    replication task can be in the replication queue.
    
    Fixes a recurring issue where persisted ref-update events were missing or
    left orphan on the filesystem due to the different granularity of the
    incoming ref-update events and the corresponding replication tasks queued
    and executed.
    
    The responsibility of persisting the events is moved from the
    ReplicationState object to the Destination.
    
    The stored objects are replication tasks and not anymore ref-update
    events.
    
    Bug: Issue 11172
    Change-Id: I02d8cda0a124e8e3d2b9bb01b7d44f98ba717fcd

--- src/test/java/com/googlesource/gerrit/plugins/replication/ReplicationIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/replication/ReplicationIT.java
@@ -21,15 +21,20 @@ import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
 import com.google.gerrit.acceptance.PushOneCommit.Result;
 import com.google.gerrit.acceptance.TestPlugin;
 import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.extensions.annotations.PluginData;
 import com.google.gerrit.extensions.common.ProjectInfo;
 import com.google.gerrit.reviewdb.client.Project;
 import com.google.gerrit.server.config.SitePaths;
 import com.google.inject.Inject;
+import com.google.inject.Key;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -42,11 +47,13 @@ import org.junit.Test;
     name = "replication",
     sysModule = "com.googlesource.gerrit.plugins.replication.ReplicationModule")
 public class ReplicationIT extends LightweightPluginDaemonTest {
-  private static final int TEST_REPLICATION_DELAY = 1;
+  private static final int TEST_REPLICATION_DELAY = 2;
   private static final Duration TEST_TIMEMOUT = Duration.ofSeconds(TEST_REPLICATION_DELAY * 10);
 
   @Inject private SitePaths sitePaths;
+  Path pluginDataDir;
   private Path gitPath;
+  private Path storagePath;
   private FileBasedConfig config;
 
   @Override
@@ -58,13 +65,20 @@ public class ReplicationIT extends LightweightPluginDaemonTest {
     gitPath = sitePaths.site_path.resolve("git");
 
     super.setUpTestPlugin();
+
+    pluginDataDir = plugin.getSysInjector().getInstance(Key.get(Path.class, PluginData.class));
+    storagePath = pluginDataDir.resolve("ref-updates");
   }
 
   @Test
   public void shouldReplicateNewProject() throws Exception {
-    setReplicationDestination("foo", "replica");
+    setReplicationDestination("foo", "replica", TEST_REPLICATION_DELAY);
 
     Project.NameKey sourceProject = createProject("foo");
+
+    String[] replicationTasks = storagePath.toFile().list();
+    assertThat(replicationTasks).hasLength(2); // refs/heads/master and /refs/meta/config
+
     waitUntil(() -> gitPath.resolve(sourceProject + "replica.git").toFile().isDirectory());
 
     ProjectInfo replicaProject = gApi.projects().name(sourceProject + "replica").get();
@@ -73,19 +87,65 @@ public class ReplicationIT extends LightweightPluginDaemonTest {
 
   @Test
   public void shouldReplicateNewBranch() throws Exception {
-    setReplicationDestination("foo", "replica");
-
     Project.NameKey targetProject = createProject("projectreplica");
+
+    setReplicationDestination("foo", "replica", TEST_REPLICATION_DELAY);
 
     Result pushResult = createChange();
     RevCommit sourceCommit = pushResult.getCommit();
     String sourceRef = pushResult.getPatchSet().getRefName();
+
+    String[] replicationTasks = storagePath.toFile().list();
+    assertThat(replicationTasks).hasLength(1);
 
     waitUntil(() -> getRef(getRepo(targetProject), sourceRef) != null);
 
     Ref targetBranchRef = getRef(getRepo(targetProject), sourceRef);
     assertThat(targetBranchRef).isNotNull();
     assertThat(targetBranchRef.getObjectId()).isEqualTo(sourceCommit.getId());
+  }
+
+  @Test
+  public void shouldReplicateNewBranchToTwoRemotes() throws Exception {
+    Project.NameKey targetProject1 = createProject("projectreplica1");
+    Project.NameKey targetProject2 = createProject("projectreplica2");
+
+    setReplicationDestination("foo1", "replica1", TEST_REPLICATION_DELAY);
+    setReplicationDestination("foo2", "replica2", TEST_REPLICATION_DELAY);
+
+    Result pushResult = createChange();
+    RevCommit sourceCommit = pushResult.getCommit();
+    String sourceRef = pushResult.getPatchSet().getRefName();
+
+    String[] replicationTasks = storagePath.toFile().list();
+    assertThat(replicationTasks).hasLength(2);
+
+    waitUntil(
+        () ->
+            (getRef(getRepo(targetProject1), sourceRef) != null
+                && getRef(getRepo(targetProject2), sourceRef) != null));
+
+    Ref targetBranchRef1 = getRef(getRepo(targetProject1), sourceRef);
+    assertThat(targetBranchRef1).isNotNull();
+    assertThat(targetBranchRef1.getObjectId()).isEqualTo(sourceCommit.getId());
+
+    Ref targetBranchRef2 = getRef(getRepo(targetProject2), sourceRef);
+    assertThat(targetBranchRef2).isNotNull();
+    assertThat(targetBranchRef2.getObjectId()).isEqualTo(sourceCommit.getId());
+  }
+
+  @Test
+  public void shouldCreateIndividualResplicationTasksForEveryRemoteUrlPair() throws Exception {
+    List<String> replicaSuffixes = Arrays.asList("replica1", "replica2");
+    createProject("projectreplica1");
+    createProject("projectreplica2");
+
+    setReplicationDestination("foo1", replicaSuffixes, TEST_REPLICATION_DELAY);
+    setReplicationDestination("foo2", replicaSuffixes, TEST_REPLICATION_DELAY);
+
+    createChange();
+
+    assertThat(storagePath.toFile().list()).hasLength(4);
   }
 
   private Repository getRepo(Project.NameKey targetProject) {
@@ -106,14 +166,19 @@ public class ReplicationIT extends LightweightPluginDaemonTest {
     }
   }
 
-  private void setReplicationDestination(String remoteName, String replicaSuffix)
-      throws IOException {
-    config.setString(
-        "remote",
-        remoteName,
-        "url",
-        gitPath.resolve("${name}" + replicaSuffix + ".git").toString());
-    config.setInt("remote", remoteName, "replicationDelay", TEST_REPLICATION_DELAY);
+  private void setReplicationDestination(
+      String remoteName, String replicaSuffix, int replicationDelay) throws IOException {
+    setReplicationDestination(remoteName, Arrays.asList(replicaSuffix), replicationDelay);
+  }
+
+  private void setReplicationDestination(
+      String remoteName, List<String> replicaSuffixes, int replicationDelay) throws IOException {
+    List<String> replicaUrls =
+        replicaSuffixes.stream()
+            .map(suffix -> gitPath.resolve("${name}" + suffix + ".git").toString())
+            .collect(Collectors.toList());
+    config.setStringList("remote", remoteName, "url", replicaUrls);
+    config.setInt("remote", remoteName, "replicationDelay", replicationDelay);
     config.save();
     reloadConfig();
   }
