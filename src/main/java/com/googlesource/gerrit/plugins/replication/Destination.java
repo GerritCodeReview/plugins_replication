62764165057d59c31c7530bf21ae37ea664a4e4e
Luca Milanesio <luca.milanesio@gmail.com>
2019-07-13 22:32:05 +0100

    Cancel pending replications upon shutdown
    When the replication plugin is stopped or reloaded,
    mark all the current replications as cancelled.
    Having pending and unmanaged replication tasks is quite
    dangerous for two reasons:
    
    1. The result of the replication isn't accounted and
       properly managed, because the associated objects won't
       be there anymore (pending replications, runway, ...)
    
    2. The destination configuration could have changed completely
       (auto-reload use-case) or even not exist anymore: the
       replication event would thus perform an unwanted and
       unconfigured operation.
    
    Change-Id: Iabc17d375011cbc61f8c642ae07d3d018b49fc69

--- src/main/java/com/googlesource/gerrit/plugins/replication/Destination.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/Destination.java
@@ -70,6 +70,7 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.eclipse.jgit.lib.Constants;
@@ -224,10 +225,30 @@ public class Destination {
   public int shutdown() {
     int cnt = 0;
     if (pool != null) {
+      foreachPushOp(
+          pending,
+          push -> {
+            push.cancel();
+            return null;
+          });
+      pending.clear();
+      foreachPushOp(
+          inFlight,
+          push -> {
+            push.setCanceledWhileRunning();
+            return null;
+          });
+      inFlight.clear();
       cnt = pool.shutdownNow().size();
       pool = null;
     }
     return cnt;
+  }
+
+  private void foreachPushOp(Map<URIish, PushOne> opsMap, Function<PushOne, Void> pushOneFunction) {
+    for (PushOne pushOne : ImmutableList.copyOf(opsMap.values())) {
+      pushOneFunction.apply(pushOne);
+    }
   }
 
   private boolean shouldReplicate(ProjectState state, CurrentUser user)
@@ -336,7 +357,7 @@ public class Destination {
     if (!config.replicatePermissions()) {
       PushOne e;
       synchronized (stateLock) {
-        e = pending.get(uri);
+        e = getPendingPush(uri);
       }
       if (e == null) {
         try (Repository git = gitManager.openRepository(project)) {
@@ -359,7 +380,7 @@ public class Destination {
     }
 
     synchronized (stateLock) {
-      PushOne e = pending.get(uri);
+      PushOne e = getPendingPush(uri);
       if (e == null) {
         e = opFactory.create(project, uri);
         addRef(e, ref);
@@ -373,6 +394,14 @@ public class Destination {
       state.increasePushTaskCount(project.get(), ref);
       repLog.info("scheduled {}:{} => {} to run after {}s", project, ref, e, config.getDelay());
     }
+  }
+
+  private PushOne getPendingPush(URIish uri) {
+    PushOne e = pending.get(uri);
+    if (e != null && !e.wasCanceled()) {
+      return e;
+    }
+    return null;
   }
 
   void pushWasCanceled(PushOne pushOp) {
@@ -419,7 +448,7 @@ public class Destination {
   void reschedule(PushOne pushOp, RetryReason reason) {
     synchronized (stateLock) {
       URIish uri = pushOp.getURI();
-      PushOne pendingPushOp = pending.get(uri);
+      PushOne pendingPushOp = getPendingPush(uri);
 
       if (pendingPushOp != null) {
         // There is one PushOp instance already pending to same URI.
