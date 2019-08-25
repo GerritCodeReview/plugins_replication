f75a345dd2d4a8900b03e8666f7e8791ca725832
Luca Milanesio <luca.milanesio@gmail.com>
2019-08-25 17:39:56 +0200

    First ReplicationIT test
    Change-Id: Iea4170aa881757c7e29bc6237fe4c230da8ae0ec

--- src/main/java/com/googlesource/gerrit/plugins/replication/AutoReloadConfigDecorator.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/AutoReloadConfigDecorator.java
@@ -13,6 +13,7 @@
 // limitations under the License.
 package com.googlesource.gerrit.plugins.replication;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.common.FileUtil;
 import com.google.gerrit.extensions.annotations.PluginData;
@@ -65,7 +66,8 @@ public class AutoReloadConfigDecorator implements ReplicationConfig {
   }
 
   private synchronized boolean isAutoReload() {
-    return currentConfig.getConfig().getBoolean("gerrit", "autoReload", false);
+    boolean autoReload = currentConfig.getConfig().getBoolean("gerrit", "autoReload", false);
+    return autoReload;
   }
 
   @Override
@@ -75,15 +77,25 @@ public class AutoReloadConfigDecorator implements ReplicationConfig {
   }
 
   private void reloadIfNeeded() {
-    if (isAutoReload()) {
+    reload(false);
+  }
+
+  @VisibleForTesting
+  public void forceReload() {
+    reload(true);
+  }
+
+  private void reload(boolean force) {
+    if (force || isAutoReload()) {
       ReplicationQueue queue = replicationQueue.get();
 
       long lastModified = getLastModified(currentConfig);
       try {
-        if (lastModified > currentConfigTs
-            && lastModified > lastFailedConfigTs
-            && queue.isRunning()
-            && !queue.isReplaying()) {
+        if (force
+            || (lastModified > currentConfigTs
+                && lastModified > lastFailedConfigTs
+                && queue.isRunning()
+                && !queue.isReplaying())) {
           queue.stop();
           currentConfig = loadConfig();
           currentConfigTs = lastModified;
