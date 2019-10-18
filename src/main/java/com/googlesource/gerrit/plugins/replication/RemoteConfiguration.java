9f4d44b62e7a03ae1496fa7b94fc0e10e23956a1
Antonio Barone <syntonyze@gmail.com>
2019-10-18 16:01:15 +0200

    Move isSingleProjectMatch() to RemoteConfiguration interface
    The logic used to identify whether a configuration is related to a
    single project as opposed to a family of project, is the same across
    destination or source targets.
    
    Provide a default  isSingleProjectMatch method so that it can be
    leveraged by both replication (for DestinationConfiguration) and by
    pull-replication (for SourceConfiguration)
    
    Change-Id: I1a366841e77755a1d17cfd9f68fe235a7a099f48

--- src/main/java/com/googlesource/gerrit/plugins/replication/RemoteConfiguration.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/RemoteConfiguration.java
@@ -14,6 +14,7 @@
 package com.googlesource.gerrit.plugins.replication;
 
 import com.google.common.collect.ImmutableList;
+import java.util.List;
 import org.eclipse.jgit.transport.RemoteConfig;
 
 /** Remote configuration for a replication endpoint */
@@ -95,4 +96,27 @@ public interface RemoteConfiguration {
    * @return the slow latency threshold
    */
   int getSlowLatencyThreshold();
+
+  /**
+   * Whether the remote configuration is for a single project only
+   *
+   * @return true, when configuration is for a single project, false otherwise
+   */
+  default boolean isSingleProjectMatch() {
+    List<String> projects = getProjects();
+    boolean ret = (projects.size() == 1);
+    if (ret) {
+      String projectMatch = projects.get(0);
+      if (ReplicationFilter.getPatternType(projectMatch)
+          != ReplicationFilter.PatternType.EXACT_MATCH) {
+        // projectMatch is either regular expression, or wild-card.
+        //
+        // Even though they might refer to a single project now, they need not
+        // after new projects have been created. Hence, we do not treat them as
+        // matching a single project.
+        ret = false;
+      }
+    }
+    return ret;
+  }
 }
