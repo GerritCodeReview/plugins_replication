2d1f1f5a56eaab4553cc051cf9b213963760ba8d
David White <david.white@ericsson.com>
2020-02-07 10:43:05 -0500

    Add REST interface for the replication start command
    Implement a REST API that is the equivalent of the ssh "start
    replication" command detailed in the existing replication plugin
    documentation. To mimic the functionality of the ssh command, keywords
    are used in the body rather than adding multiple REST endpoints.
    
    Change-Id: I79a3ef0f78fe684150fb6b8915851af412eeaa40

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/EventRestApiServlet.java
@@ -0,0 +1,93 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import static com.googlesource.gerrit.plugins.replication.ReplicationQueue.repLog;
+
+import com.google.gerrit.extensions.annotations.Export;
+import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.BufferedReader;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiresCapability(StartReplicationCapability.START_REPLICATION)
+@Export("/start")
+@Singleton
+public class EventRestApiServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  private StartReplicationRequest startReplicationRequest;
+
+  private final StartReplicationProcessing startReplicationProcessing;
+
+  @Inject
+  EventRestApiServlet(
+      StartReplicationProcessing startReplicationProcessing, StartReplicationRequest startReplicationRequest) {
+    this.startReplicationProcessing = startReplicationProcessing;
+    this.startReplicationRequest = startReplicationRequest;
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse res) {
+    if (!setStartReplicationRequest(req)) {
+      try {
+        res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+      } catch (IOException ioException) {
+        repLog.error("Problem sending servlet response {}", ioException.getLocalizedMessage());
+      }
+      return;
+    }
+
+    repLog.debug(
+        "Received command={}, project={}, url={}, now={}, wait={}",
+        startReplicationRequest.getCommand(),
+        startReplicationRequest.getProject(),
+        startReplicationRequest.getUrl(),
+        startReplicationRequest.isNow(),
+        startReplicationRequest.isWait());
+
+    try {
+      startReplicationProcessing.execute(startReplicationRequest, res);
+    } catch (IOException e) {
+      repLog.error("Problem executing replication {}", e.getLocalizedMessage());
+    }
+  }
+
+  private boolean setStartReplicationRequest(HttpServletRequest request) {
+    try {
+      Gson gson = new Gson();
+      BufferedReader reader = request.getReader();
+      startReplicationRequest = gson.fromJson(reader, StartReplicationRequest.class);
+    } catch (JsonParseException | IOException e) {
+      return false;
+    }
+
+    // Validate command as it is mandatory. If set to project, ensure project is set as well
+    // Remaining fields are optional
+
+    String command = startReplicationRequest.getCommand();
+    if (command != null) {
+      return (command.matches("all")
+          || (command.matches("project") && (startReplicationRequest.getProject() != null)));
+    }
+    return false;
+  }
+}
