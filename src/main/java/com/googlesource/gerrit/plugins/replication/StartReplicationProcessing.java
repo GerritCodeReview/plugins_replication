36170b9295a27034408b7cbc2aa5d303d6632879
David White <david.white@ericsson.com>
2020-06-10 08:58:06 -0400

    Add REST interface for the replication start command
    Implement a REST API that is the equivalent of the ssh "start
    replication" command detailed in the existing replication plugin
    documentation. To mimic the functionality of the ssh command, keywords
    are used in the body rather than adding multiple REST endpoints.
    
    Change-Id: I79a3ef0f78fe684150fb6b8915851af412eeaa40

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/StartReplicationProcessing.java
@@ -0,0 +1,112 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import static org.apache.http.HttpStatus.SC_NO_CONTENT;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.replication.PushResultProcessing.ServletCommandProcessing;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.http.HttpServletResponse;
+
+public class StartReplicationProcessing {
+
+  enum CommandType {
+    ALL,
+    PROJECT
+  }
+
+  private final PushAll.Factory pushFactory;
+
+  @Inject private ReplicationStateLogger stateLogger;
+
+  @Inject
+  StartReplicationProcessing(PushAll.Factory pushFactory) {
+    this.pushFactory = pushFactory;
+  }
+
+  @VisibleForTesting
+  public void execute(
+      StartReplicationRequest startReplicationRequest,
+      HttpServletResponse response,
+      ReplicationStateLogger stateLogger)
+      throws IOException {
+    this.stateLogger = stateLogger;
+    execute(startReplicationRequest, response);
+  }
+
+  public void execute(StartReplicationRequest startReplicationRequest, HttpServletResponse response)
+      throws IOException {
+    CommandType cmd = startReplicationRequest.getCommand();
+    ReplicationFilter projectFilter;
+
+    switch (cmd) {
+      case ALL:
+        projectFilter = ReplicationFilter.all();
+        break;
+
+      case PROJECT:
+        List<String> projectPatterns = new ArrayList<>();
+        projectPatterns.add(startReplicationRequest.getProject());
+        projectFilter = new ReplicationFilter(projectPatterns);
+        break;
+
+      default:
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        return;
+    }
+
+    response.setStatus(SC_NO_CONTENT);
+    String urlMatch = startReplicationRequest.getUrl();
+    ReplicationState state = new ReplicationState(new ServletCommandProcessing());
+
+    Future<?> future =
+        pushFactory
+            .create(urlMatch, projectFilter, state, startReplicationRequest.isNow())
+            .schedule(0, TimeUnit.SECONDS);
+
+    // This pattern mirrors the processing in the ssh version of the command.
+    if (startReplicationRequest.isWait()) {
+      if (future != null) {
+        try {
+          future.get();
+        } catch (InterruptedException e) {
+          stateLogger.error(
+              "Thread was interrupted while waiting for PushAll operation to finish", e, state);
+          return;
+        } catch (ExecutionException e) {
+          stateLogger.error("An exception was thrown in PushAll operation", e, state);
+          return;
+        }
+      }
+
+      if (state.hasPushTask()) {
+        try {
+          state.waitForReplication();
+        } catch (InterruptedException e) {
+          response.sendError(
+              HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+              "Thread was interrupted while waiting for replication to complete");
+        }
+      }
+    }
+  }
+}
