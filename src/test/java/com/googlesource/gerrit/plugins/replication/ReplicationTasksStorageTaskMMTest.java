871ba5c0e9d037bd919df5e4cdf5a5d0a87a93c6
Adithya Chakilam <achakila@codeaurora.org>
2020-08-05 15:56:20 -0500

    ReplicationTasksStorage.Task: Add multi-master unit tests
    These tests examine the replication scenarios under
    multi-master setup making use of the api calls present
    in ReplicationTasksStorage.Task class similarly as done in
    single master setup.
    
    Change-Id: I980e8286bf11d31c6ab89e49ef065fdde1118181

---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/ReplicationTasksStorageTaskMMTest.java
@@ -0,0 +1,229 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+import org.eclipse.jgit.transport.URIish;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReplicationTasksStorageTaskMMTest {
+  protected static final String PROJECT = "myProject";
+  protected static final String REF = "myRef";
+  protected static final String REMOTE = "myDest";
+  protected static final URIish URISH =
+      ReplicationTasksStorageTest.getUrish("http://example.com/" + PROJECT + ".git");
+  protected static final ReplicationTasksStorage.ReplicateRefUpdate REF_UPDATE =
+      new ReplicationTasksStorage.ReplicateRefUpdate(PROJECT, REF, URISH, REMOTE);
+
+  protected FileSystem fileSystem;
+  protected Path storageSite;
+  protected ReplicationTasksStorage nodeA;
+  protected ReplicationTasksStorage nodeB;
+  protected ReplicationTasksStorage.Task taskA;
+  protected ReplicationTasksStorage.Task taskB;
+  protected ReplicationTasksStorage persistedView;
+
+  @Before
+  public void setUp() throws Exception {
+    fileSystem = Jimfs.newFileSystem(Configuration.unix());
+    storageSite = fileSystem.getPath("replication_site");
+    nodeA = new ReplicationTasksStorage(storageSite);
+    nodeB = new ReplicationTasksStorage(storageSite);
+    persistedView = new ReplicationTasksStorage(storageSite);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    persistedView = null;
+    nodeB = null;
+    nodeA = null;
+    storageSite = null;
+    fileSystem.close();
+  }
+
+  @Test
+  public void waitingTaskCanBeCompletedByOtherNode() {
+    taskA.create();
+
+    taskB.start();
+    ReplicationTasksStorageTaskTest.assertIsRunning(taskA);
+
+    taskB.finish();
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskA);
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskA);
+  }
+
+  @Test
+  public void resetTaskCanBeCompletedByOtherNode() {
+    taskA.create();
+    taskA.start();
+
+    taskA.reset();
+    ReplicationTasksStorageTaskTest.assertIsWaiting(taskA);
+
+    taskB.start();
+    ReplicationTasksStorageTaskTest.assertIsRunning(taskA);
+
+    taskB.finish();
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskA);
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskA);
+  }
+
+  @Test
+  public void retryCanBeRetriedAndCompletedByOtherNode() {
+    taskA.create();
+    taskA.start();
+    taskA.reset();
+    taskB.start();
+
+    taskB.reset();
+    ReplicationTasksStorageTaskTest.assertIsWaiting(taskA);
+
+    taskB.start();
+    ReplicationTasksStorageTaskTest.assertIsRunning(taskA);
+
+    taskB.finish();
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskA);
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskA);
+  }
+
+  @Test
+  public void retryCanBeRetriedOtherNodeAndCompletedByOriginalNode() {
+    taskA.create();
+    taskA.start();
+    taskA.reset();
+    taskB.start();
+    taskB.reset();
+
+    taskA.start();
+    ReplicationTasksStorageTaskTest.assertIsRunning(taskA);
+
+    taskA.finish();
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskA);
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskA);
+  }
+
+  @Test
+  public void canBeResetAllAndCompletedByOtherNode() {
+    taskA.create();
+    taskA.start();
+
+    nodeB.resetAll();
+    ReplicationTasksStorageTaskTest.assertIsWaiting(taskA);
+
+    taskB.start();
+    ReplicationTasksStorageTaskTest.assertIsRunning(taskA);
+
+    taskA.finish();
+    //  Bug: https://crbug.com/gerrit/12973
+    // ReplicationTasksStorageTaskTest.assertIsRunning(taskB);
+
+    taskB.finish();
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskA);
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskA);
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskB);
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskB);
+  }
+
+  @Test
+  public void canBeResetAllAndCompletedByOtherNodeFastOriginalNode() {
+    taskA.create();
+    taskA.start();
+    nodeB.resetAll();
+
+    taskA.finish();
+    ReplicationTasksStorageTaskTest.assertIsWaiting(taskA);
+
+    taskB.start();
+    ReplicationTasksStorageTaskTest.assertIsRunning(taskA);
+
+    taskB.finish();
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskA);
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskA);
+  }
+
+  @Test
+  public void canBeResetAllAndCompletedByOtherNodeSlowOriginalNode() {
+    taskA.create();
+    taskA.start();
+    nodeB.resetAll();
+
+    taskB.start();
+    ReplicationTasksStorageTaskTest.assertIsRunning(taskA);
+
+    taskB.finish();
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskA);
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskA);
+
+    taskA.finish();
+    ReplicationTasksStorageTaskTest.assertNotWaiting(taskA);
+    ReplicationTasksStorageTaskTest.assertNotRunning(taskA);
+  }
+
+  @Test(expected = Test.None.class /* no exception expected */)
+  public void illegalRunningTaskResetByOtherNodeIsGraceful() {
+    taskA.create();
+    taskA.start();
+    taskB.reset();
+  }
+
+  @Test(expected = Test.None.class /* no exception expected */)
+  public void illegalRunningTaskFinishByOtherNodeIsGraceful() {
+    taskA.create();
+    taskA.start();
+    taskB.finish();
+  }
+
+  @Test(expected = Test.None.class /* no exception expected */)
+  public void illegalResetTaskResetByOtherNodeIsGraceful() {
+    taskA.create();
+    taskA.start();
+    taskA.reset();
+    taskB.reset();
+  }
+
+  @Test(expected = Test.None.class /* no exception expected */)
+  public void illegalFinishByOtherNodeIsGraceful() {
+    taskA.create();
+    taskA.start();
+    taskA.finish();
+    taskB.finish();
+  }
+
+  @Test(expected = Test.None.class /* no exception expected */)
+  public void illegalFinishByOtherNodeAfterResetIsGraceful() {
+    taskA.create();
+    taskA.start();
+
+    taskA.reset();
+    ReplicationTasksStorageTaskTest.assertIsWaiting(taskA);
+
+    taskB.finish();
+    ReplicationTasksStorageTaskTest.assertIsWaiting(taskA);
+  }
+
+  @Test(expected = Test.None.class /* no exception expected */)
+  public void illegalResetByOtherNodeAfterFinishIsGraceful() {
+    taskA.create();
+    taskA.start();
+    taskA.finish();
+    taskB.reset();
+  }
+}
