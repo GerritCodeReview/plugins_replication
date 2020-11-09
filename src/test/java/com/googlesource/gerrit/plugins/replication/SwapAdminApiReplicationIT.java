5b0b1f609455d23ca8ef74d49db7779233b1afcc
Jacek Centkowski <jcentkowski@digital.ai>
2020-11-09 19:41:35 +0100

    Add integration test for AdminApiFactory swap
    SwapAdminApiReplicationIT demonstrates how to swap AdminApiFactory
    with the Destination extension and own version of ReplicationModule.
    Extension of ReplicationModule seems like a quick win but when
    replication is used as a library it typically requires more changes
    than just this binding hence I have decided to introduce a dedicated
    implementation. Yet from maintenance perspective it requires more
    effort.
    
    Change-Id: I13dcb4e41db9c48335fd32e353221f4e3f05140b

---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/SwapAdminApiReplicationIT.java
@@ -0,0 +1,151 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.api.projects.BranchInput;
+import com.google.gerrit.extensions.registration.DynamicItem;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.PluginUser;
+import com.google.gerrit.server.account.GroupBackend;
+import com.google.gerrit.server.account.GroupIncludeCache;
+import com.google.gerrit.server.events.EventDispatcher;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Provider;
+import com.google.inject.assistedinject.Assisted;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.URIish;
+import org.junit.Test;
+
+@UseLocalDisk
+@TestPlugin(
+    name = "replication",
+    sysModule = "com.googlesource.gerrit.plugins.replication.SwapAdminApiReplicationModule")
+public class SwapAdminApiReplicationIT extends ReplicationDaemon {
+  private static final int TEST_REPLICATION_DELAY = 1;
+  private static final int TEST_REPLICATION_RETRY = 1;
+  private static final Duration TEST_TIMEOUT =
+      Duration.ofSeconds((TEST_REPLICATION_DELAY + TEST_REPLICATION_RETRY * 60) + 1);
+  private static final AtomicInteger TEST_FACTORY_CALLED = new AtomicInteger(0);
+
+  @Override
+  public void setUpTestPlugin() throws Exception {
+    initConfig();
+    setReplicationDestination("remote1", "suffix1", Optional.of("not-used-project"));
+    super.setUpTestPlugin();
+  }
+
+  @Test
+  public void shouldUseTestAdminApiFactory() throws Exception {
+    setReplicationDestination("foo", "replica", ALL_PROJECTS);
+    reloadConfig();
+
+    Project.NameKey targetProject = createTestProject(project + "replica");
+    String newHead = "refs/heads/newhead";
+    String master = "refs/heads/master";
+    BranchInput input = new BranchInput();
+    input.revision = master;
+    gApi.projects().name(project.get()).branch(newHead).create(input);
+    gApi.projects().name(project.get()).head(newHead);
+
+    try (Repository repo = repoManager.openRepository(targetProject)) {
+      waitUntil(() -> checkedGetRef(repo, newHead) != null);
+
+      Ref targetProjectHead = getRef(repo, Constants.HEAD);
+      assertThat(targetProjectHead).isNotNull();
+      assertThat(targetProjectHead.getTarget().getName()).isEqualTo(newHead);
+    }
+    assertThat(TEST_FACTORY_CALLED.get()).isGreaterThan(0);
+  }
+
+  private void waitUntil(Supplier<Boolean> waitCondition) throws InterruptedException {
+    WaitUtil.waitUntil(waitCondition, TEST_TIMEOUT);
+  }
+
+  /**
+   * Factory delegates to the DefaultAdminApiFactory but it increases TEST_FACTORY_CALLED counter
+   * each time when AdminApi is created.
+   */
+  static class TestAdminApiFactory implements AdminApiFactory {
+    private final DefaultAdminApiFactory defaultFactry;
+
+    @Inject
+    TestAdminApiFactory(DefaultAdminApiFactory defaultFactry) {
+      this.defaultFactry = defaultFactry;
+    }
+
+    @Override
+    public Optional<AdminApi> create(URIish uri) {
+      TEST_FACTORY_CALLED.incrementAndGet();
+      return defaultFactry.create(uri);
+    }
+  }
+
+  /** Swaps DefaultAdminApiFactory with TestAdminApiFactory */
+  static class TestDestination extends Destination {
+    interface Factory extends Destination.Factory {
+      @Override
+      TestDestination create(DestinationConfiguration cfg);
+    }
+
+    @Inject
+    protected TestDestination(
+        Injector injector,
+        PluginUser pluginUser,
+        GitRepositoryManager gitRepositoryManager,
+        PermissionBackend permissionBackend,
+        Provider<CurrentUser> userProvider,
+        ProjectCache projectCache,
+        GroupBackend groupBackend,
+        ReplicationStateListeners stateLog,
+        GroupIncludeCache groupIncludeCache,
+        DynamicItem<EventDispatcher> eventDispatcher,
+        Provider<ReplicationTasksStorage> rts,
+        @Assisted DestinationConfiguration cfg) {
+      super(
+          injector,
+          pluginUser,
+          gitRepositoryManager,
+          permissionBackend,
+          userProvider,
+          projectCache,
+          groupBackend,
+          stateLog,
+          groupIncludeCache,
+          eventDispatcher,
+          rts,
+          cfg);
+    }
+
+    @Override
+    protected Class<? extends AdminApiFactory> getAdminApiFactory() {
+      return TestAdminApiFactory.class;
+    }
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/SwapAdminApiReplicationModule.java
@@ -0,0 +1,134 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import static com.googlesource.gerrit.plugins.replication.StartReplicationCapability.START_REPLICATION;
+
+import com.google.common.eventbus.EventBus;
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.extensions.config.CapabilityDefinition;
+import com.google.gerrit.extensions.events.GitReferenceUpdatedListener;
+import com.google.gerrit.extensions.events.HeadUpdatedListener;
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.extensions.events.ProjectDeletedListener;
+import com.google.gerrit.extensions.registration.DynamicSet;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.gerrit.server.events.EventTypes;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.ProvisionException;
+import com.google.inject.Scopes;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.internal.UniqueAnnotations;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.util.FS;
+
+/**
+ * This a copy of ReplicationModule. The only difference is in Destination.Factory interface
+ * binding: it is bound to TestDestination.Factory so that TestDestination is created instead of a
+ * default Destination. Module has to be defined in its own file as otherwise it is not found.
+ */
+class SwapAdminApiReplicationModule extends AbstractModule {
+  private final SitePaths site;
+  private final Path cfgPath;
+
+  @Inject
+  public SwapAdminApiReplicationModule(SitePaths site) {
+    this.site = site;
+    this.cfgPath = site.etc_dir.resolve("replication.config");
+  }
+
+  @Override
+  protected void configure() {
+    install(
+        new FactoryModuleBuilder().build(SwapAdminApiReplicationIT.TestDestination.Factory.class));
+    bind(Destination.Factory.class).to(SwapAdminApiReplicationIT.TestDestination.Factory.class);
+
+    // from this point it should equal to ReplicationModule
+    bind(ReplicationQueue.class).in(Scopes.SINGLETON);
+    bind(ObservableQueue.class).to(ReplicationQueue.class);
+    bind(LifecycleListener.class)
+        .annotatedWith(UniqueAnnotations.create())
+        .to(ReplicationQueue.class);
+
+    DynamicSet.bind(binder(), GitReferenceUpdatedListener.class).to(ReplicationQueue.class);
+    DynamicSet.bind(binder(), ProjectDeletedListener.class).to(ReplicationQueue.class);
+    DynamicSet.bind(binder(), HeadUpdatedListener.class).to(ReplicationQueue.class);
+
+    bind(OnStartStop.class).in(Scopes.SINGLETON);
+    bind(LifecycleListener.class).annotatedWith(UniqueAnnotations.create()).to(OnStartStop.class);
+    bind(LifecycleListener.class)
+        .annotatedWith(UniqueAnnotations.create())
+        .to(ReplicationLogFile.class);
+    bind(CredentialsFactory.class)
+        .to(AutoReloadSecureCredentialsFactoryDecorator.class)
+        .in(Scopes.SINGLETON);
+    bind(CapabilityDefinition.class)
+        .annotatedWith(Exports.named(START_REPLICATION))
+        .to(StartReplicationCapability.class);
+
+    install(new FactoryModuleBuilder().build(PushAll.Factory.class));
+
+    bind(EventBus.class).in(Scopes.SINGLETON);
+    bind(ReplicationDestinations.class).to(DestinationsCollection.class);
+    bind(ConfigParser.class).to(DestinationConfigParser.class).in(Scopes.SINGLETON);
+
+    if (getReplicationConfig().getBoolean("gerrit", "autoReload", false)) {
+      bind(ReplicationConfig.class)
+          .annotatedWith(MainReplicationConfig.class)
+          .to(getReplicationConfigClass());
+      bind(ReplicationConfig.class).to(AutoReloadConfigDecorator.class).in(Scopes.SINGLETON);
+      bind(LifecycleListener.class)
+          .annotatedWith(UniqueAnnotations.create())
+          .to(AutoReloadConfigDecorator.class);
+    } else {
+      bind(ReplicationConfig.class).to(getReplicationConfigClass()).in(Scopes.SINGLETON);
+    }
+
+    DynamicSet.setOf(binder(), ReplicationStateListener.class);
+    DynamicSet.bind(binder(), ReplicationStateListener.class).to(ReplicationStateLogger.class);
+
+    EventTypes.register(RefReplicatedEvent.TYPE, RefReplicatedEvent.class);
+    EventTypes.register(RefReplicationDoneEvent.TYPE, RefReplicationDoneEvent.class);
+    EventTypes.register(ReplicationScheduledEvent.TYPE, ReplicationScheduledEvent.class);
+    bind(SshSessionFactory.class).toProvider(ReplicationSshSessionFactoryProvider.class);
+
+    bind(TransportFactory.class).to(TransportFactoryImpl.class).in(Scopes.SINGLETON);
+  }
+
+  private FileBasedConfig getReplicationConfig() {
+    File replicationConfigFile = cfgPath.toFile();
+    FileBasedConfig config = new FileBasedConfig(replicationConfigFile, FS.DETECTED);
+    try {
+      config.load();
+    } catch (IOException | ConfigInvalidException e) {
+      throw new ProvisionException("Unable to load " + replicationConfigFile.getAbsolutePath(), e);
+    }
+    return config;
+  }
+
+  private Class<? extends ReplicationConfig> getReplicationConfigClass() {
+    if (Files.exists(site.etc_dir.resolve("replication"))) {
+      return FanoutReplicationConfig.class;
+    }
+    return ReplicationFileBasedConfig.class;
+  }
+}
