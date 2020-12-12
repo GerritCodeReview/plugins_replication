9c10e116217759166a5e66a00c0fa9607d90d123
Luca Milanesio <luca.milanesio@gmail.com>
2020-12-12 00:55:53 +0000

    Rely on WaitUtil moved to the acceptance framework
    WaitUtil has been moved to the acceptance framework
    in Gerrit core.
    
    Change-Id: I3a31335c7878a9e5b9082d6685b860e8e6c42325

--- src/test/java/com/googlesource/gerrit/plugins/replication/ReplicationStorageIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/replication/ReplicationStorageIT.java
@@ -20,6 +20,7 @@ import static java.util.stream.Collectors.toList;
 
 import com.google.gerrit.acceptance.TestPlugin;
 import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.acceptance.WaitUtil;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.extensions.api.projects.BranchInput;
 import com.googlesource.gerrit.plugins.replication.Destination.QueueInfo;
