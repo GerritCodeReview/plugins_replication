7fc8193fb7ecef7c6c2a61acb43390800b6082df
Antonio Barone <syntonyze@gmail.com>
2021-07-23 14:31:25 +0200

    Remote replication events inherit from parent class
    The replication plugin models replication events that are directed to a
    specific remote.
    
    These are:
    
    RefReplicatedEvent and ReplicationScheduledEvent
    
    These events share many fields (such as project, ref, target) and this
    makes sense, as they all represent the status of a ref replication to a
    particular remote.
    
    However, rather than inheriting from a common parent, most fields are
    copy & pasted redundantly into each event.
    
    Factor out common fields by having RefReplicatedEvent and
    ReplicationScheduledEvent from a common parent.
    
    This also opens the door for other ref-replication-based events (such as
    pull-replication for example) to adhere to the same interface.
    
    Bug: Issue 14819
    Change-Id: I29da700bf6671159bd6ae29b00ef6b962ef0fcb5

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/events/RemoteReplicationRefEvent.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.events;
+
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.server.events.RefEvent;
+import org.eclipse.jgit.transport.URIish;
+
+public class RemoteReplicationRefEvent extends RefEvent {
+
+  public final String project;
+  public final String ref;
+  public final String status;
+  public final String targetUri;
+
+  public RemoteReplicationRefEvent(
+      String type, String project, String ref, URIish targetUri, @Nullable String status) {
+    super(type);
+    this.project = project;
+    this.ref = ref;
+    this.status = status;
+    this.targetUri = targetUri.toASCIIString();
+  }
+
+  @Override
+  public Project.NameKey getProjectNameKey() {
+    return Project.nameKey(project);
+  }
+
+  @Override
+  public String getRefName() {
+    return ref;
+  }
+}
