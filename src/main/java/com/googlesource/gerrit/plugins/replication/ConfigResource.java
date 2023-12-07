072f03b7ac48c780d953c9bd4d5190aaa0d26d08
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-12-07 10:59:20 +0000

    Add `ConfigResource` interface
    This is a first step for providing replication configuration out of the
    server file system. The `ConfigResource` bundles togeher JGit `Config`
    instance with its version.
    
    The future implementations can read the configuration from a Gerrit
    project or external systems like ZooKeeper.
    
    Additinally, the `ConfigResource` separates the "configuration source"
    from "configuration schema". The "schema" is provided by the
    `ReplicationFileBasedConfig` that is hardcoding section and property
    names used to get the configuraiton values from JGit `Config` object.
    Whereas the `ConfigResource` interface is responsible for getting
    the whole configuration object from somewhere. It maybe from the file
    system, a remote system or multiple files.
    
    Change-Id: I77d205dce223e508bf4a06c5ff9da91444a87032

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ConfigResource.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication;
+
+import org.eclipse.jgit.lib.Config;
+
+public interface ConfigResource {
+
+  /**
+   * Configuration for the plugin and all replication end points.
+   *
+   * @return current configuration
+   */
+  Config getConfig();
+
+  /**
+   * Current logical version string of the current configuration loaded in memory, depending on the
+   * actual implementation of the configuration on the persistent storage.
+   *
+   * @return current logical version number.
+   */
+  String getVersion();
+}
