0ecf005910384d3ed3bd343458bb659c07edf92e
Edwin Kempin <ekempin@google.com>
2024-01-03 15:22:30 +0000

    Adapt to enabling the MutableGuiceModule errorprone pattern in Gerrit core
    Change-Id: I839505a493f8cc03ee598d3cf076254bfd50b006
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- src/main/java/com/googlesource/gerrit/plugins/replication/SshModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/SshModule.java
@@ -14,9 +14,16 @@
 
 package com.googlesource.gerrit.plugins.replication;
 
+import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.sshd.PluginCommandModule;
+import com.google.inject.Inject;
 
 class SshModule extends PluginCommandModule {
+  @Inject
+  SshModule(@PluginName String pluginName) {
+    super(pluginName);
+  }
+
   @Override
   protected void configureCommands() {
     command(StartCommand.class);
