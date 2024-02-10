14d6debd35d16af74fbc2666b60646cb0916f48a
Luca Milanesio <luca.milanesio@gmail.com>
2024-02-10 21:55:12 +0000

    Decouple replication-api.jar from the main replication plugin
    The Change Ib7a04eea5 made the whole replication plugin an ApiModule
    which becomes the base injector for all plugins and forbids reloads,
    even when there is no intention to use th extension points.
    
    All plugins that expose an ApiModule have the following limitations:
    - Cannot add extra dependencies
    - Cannot be reloaded
    - Are the base injectors for all plugins
    
    The above limitations aren't acceptable for the whole replication
    plugin when used standalone without extensions.
    
    Define a new replication-api target that generates the sole ApiModule
    needed for extending the replication plugin functionality.
    All the classes that are part of the replication-api are moved
    into a separate c.g.g.p.replication.api package so that can be
    included in the replication-api.jar plugin.
    
    The gerrit.war will continue to include only the replication.jar which
    will still work out of the box. Anyone willing to customise its behaviour
    through the API will have to build the replication-api target.
    
    Bug: Issue 324462734
    Release-Notes: Decouple replication-api.jar from the main replication plugin
    Change-Id: Ic432fc77daf1162368abd65f64c463e4ef4f5d6d

--- src/main/java/com/googlesource/gerrit/plugins/replication/AutoReloadRunnable.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/AutoReloadRunnable.java
@@ -18,6 +18,7 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.flogger.FluentLogger;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.googlesource.gerrit.plugins.replication.api.ReplicationConfig;
 import java.util.List;
 
 public class AutoReloadRunnable implements Runnable {
