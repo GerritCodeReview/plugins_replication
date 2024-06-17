0acb2d9269846ad33549bbda78f1bf79aa592ba5
Luca Milanesio <luca.milanesio@gmail.com>
2024-06-17 08:53:34 +0100

    Introduce API for retrieving the remote config
    Extend the ReplicationRemotesApi for retrieving the
    configuration associated with a replication remote.
    
    Change-Id: I26b6f098dd7b6331adf929315b2cc1cebdf0eb4a

--- src/main/java/com/googlesource/gerrit/plugins/replication/api/ReplicationRemotesApi.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/api/ReplicationRemotesApi.java
@@ -28,6 +28,19 @@ import org.eclipse.jgit.lib.Config;
 public interface ReplicationRemotesApi {
 
   /**
+   * Retrieves the configuration for a remote by name.
+   *
+   * <p>Builds a JGit {@link Config} with the remote's configuration obtained by parsing the
+   * replication configuration sources.
+   *
+   * <p>NOTE: The remotes secrets are excluded for security reasons.
+   *
+   * @param remoteName the remote name to retrieve.
+   * @return {@link Config} associated with the remoteName.
+   */
+  Config get(String remoteName);
+
+  /**
    * Adds or updates the remote configuration for the replication plugin.
    *
    * <p>Provided JGit {@link Config} object should contain at least one named <em>remote</em>
