1a770068f5037cb613e80d2a0abffbb3d451f341
paladox <thomasmulhall410@yahoo.com>
2025-08-31 17:28:02 +0100

    Switch logger to log4j2
    We're switching from log4j to log4j2 in gerrit.
    Adapt to support it.
    
    Depends-On: I470078dd0dba0936c56c9936f6644723d486197d
    Change-Id: I939b7f992f17991cd5ef341048fcef186c5a41d3

--- src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationLogFile.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationLogFile.java
@@ -22,9 +22,11 @@ import com.google.gerrit.util.logging.JsonLayout;
 import com.google.gerrit.util.logging.JsonLogEntry;
 import com.google.gson.annotations.SerializedName;
 import com.google.inject.Inject;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.spi.LoggingEvent;
+import java.nio.charset.StandardCharsets;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
+/** Plugin log file for replication messages. */
 public class ReplicationLogFile extends PluginLogFile {
 
   @Inject
@@ -33,29 +35,37 @@ public class ReplicationLogFile extends PluginLogFile {
         systemLog,
         serverInfo,
         ReplicationQueue.REPLICATION_LOG_NAME,
-        new PatternLayout("[%d] %m%n"),
-        new ReplicationJsonLayout(),
+        PatternLayout.newBuilder()
+            .withPattern("[%d] %m%n")
+            .withCharset(StandardCharsets.UTF_8)
+            .build(),
+        new ReplicationJsonLayout(StandardCharsets.UTF_8),
         config);
   }
 
   static class ReplicationJsonLayout extends JsonLayout {
+
+    public ReplicationJsonLayout(java.nio.charset.Charset charset) {
+      super(charset); // required by Log4j2 JsonLayout constructor
+    }
+
     @SuppressWarnings("unused")
-    private class ReplicationJsonLogEntry extends JsonLogEntry {
-      public String timestamp;
-      public String message;
+    private static class ReplicationJsonLogEntry extends JsonLogEntry {
+      public final String timestamp;
+      public final String message;
 
       @SerializedName("@version")
       public final int version = 1;
 
-      public ReplicationJsonLogEntry(LoggingEvent event) {
-        timestamp = timestampFormatter.format(event.getTimeStamp());
-        message = (String) event.getMessage();
+      ReplicationJsonLogEntry(LogEvent event, com.google.gerrit.util.logging.LogTimestampFormatter formatter) {
+        this.timestamp = formatter.format(event.getTimeMillis());
+        this.message = event.getMessage().getFormattedMessage();
       }
     }
 
     @Override
-    public JsonLogEntry toJsonLogEntry(LoggingEvent event) {
-      return new ReplicationJsonLogEntry(event);
+    public JsonLogEntry toJsonLogEntry(LogEvent event) {
+      return new ReplicationJsonLogEntry(event, this.timestampFormatter);
     }
   }
 }
