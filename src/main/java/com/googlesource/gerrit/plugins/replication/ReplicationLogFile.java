17989e7e4c5ea10699a912ae71af373a22c636ef
paladox <thomasmulhall410@yahoo.com>
2025-08-31 17:28:02 +0100

    Switch logger to log4j2
    We're switching from log4j to log4j2 in gerrit.
    Adapt to support it.
    
    Depends-On: I470078dd0dba0936c56c9936f6644723d486197d
    Change-Id: I939b7f992f17991cd5ef341048fcef186c5a41d3

--- src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationLogFile.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/ReplicationLogFile.java
@@ -22,23 +22,35 @@ import com.google.gerrit.util.logging.JsonLayout;
 import com.google.gerrit.util.logging.JsonLogEntry;
 import com.google.gson.annotations.SerializedName;
 import com.google.inject.Inject;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.spi.LoggingEvent;
+import java.nio.charset.StandardCharsets;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
 public class ReplicationLogFile extends PluginLogFile {
 
   @Inject
-  public ReplicationLogFile(SystemLog systemLog, ServerInformation serverInfo, LogConfig config) {
+  public ReplicationLogFile(
+      SystemLog systemLog, ServerInformation serverInfo, LogConfig config) {
     super(
         systemLog,
         serverInfo,
         ReplicationQueue.REPLICATION_LOG_NAME,
-        new PatternLayout("[%d] %m%n"),
+        PatternLayout.newBuilder().withPattern("[%d] %m%n").build(),
         new ReplicationJsonLayout(),
         config);
   }
 
   static class ReplicationJsonLayout extends JsonLayout {
+
+    public ReplicationJsonLayout() {
+      super(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public JsonLogEntry toJsonLogEntry(LogEvent event) {
+      return new ReplicationJsonLogEntry(event);
+    }
+
     @SuppressWarnings("unused")
     private class ReplicationJsonLogEntry extends JsonLogEntry {
       public String timestamp;
@@ -47,15 +59,10 @@ public class ReplicationLogFile extends PluginLogFile {
       @SerializedName("@version")
       public final int version = 1;
 
-      public ReplicationJsonLogEntry(LoggingEvent event) {
-        timestamp = timestampFormatter.format(event.getTimeStamp());
-        message = (String) event.getMessage();
+      public ReplicationJsonLogEntry(LogEvent event) {
+        timestamp = timestampFormatter.format(event.getTimeMillis());
+        message = event.getMessage().getFormattedMessage();
       }
-    }
-
-    @Override
-    public JsonLogEntry toJsonLogEntry(LoggingEvent event) {
-      return new ReplicationJsonLogEntry(event);
     }
   }
 }
